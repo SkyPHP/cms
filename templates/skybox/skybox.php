deea1b24ef17231baedf69838752e4110c18979b
Stan Rozenraukh <stan@jooonbug.com>
2011-03-29 12:24:53 -0400

    added skybox template, tinymce, close-box image

---
+++ templates/skybox/skybox.php
@@ -0,0 +1,18 @@
+<?
+if ( $template_area == 'top' ):
+?>
+<div id="skybox_template" class="has-floats" style="border: 2px #333 solid; padding: 15px;">
+	<div id="skybox_template_title" style="float:left; font-weight:bold;">
+		<?=$this->title?>
+	</div>
+	<div id="skybox_template_close" style="float:right;">
+		<a href="javascript:closeskybox()"><img src="/images/close-x.gif"></a>
+	</div>
+	<div id="skybox_template_title_clear" style="clear:both; height: 15px;"></div>
+<?
+elseif ( $template_area == 'bottom' ):
+?>
+</div>
+<?
+endif;
+?>
