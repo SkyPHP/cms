a1fc93525324ae236cf58b97773dff4b72c931e6
Stan Rozenraukh <stan@jooonbug.com>
2011-04-07 13:48:06 -0400

    json fixes

--- pages/save/v2/v2.php
+++ pages/save/v2/v2.php
@@ -4,4 +4,6 @@ if (!preg_match('/^[\w0-9]+$/', $model_name)) exit;
 if (!$_POST) exit;
 $m = model::get($model_name);
 $m->loadArray($_POST);
-$m->save();
+$arr = $m->save();
+json_headers();
+exit(json_encode($arr));
