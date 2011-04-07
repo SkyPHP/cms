c8f6e8600fed5d2f0744a8e1805fa34bf69043dc
zgr024 <zach@joonbug.com>
2011-04-07 16:32:33 -0400

    css

--- pages/admin/seo/webpage/webpage-skybox.php
+++ pages/admin/seo/webpage/webpage-skybox.php
@@ -1,8 +1,10 @@
 <?
 	$_POST['page_path']="pages/admin/seo/webpage/webpage.php";
 	$p->title="SEO";
-	$p->css[]="/pages/admin/seo/webpage/webpage.css";
 	$p->template('skybox','top');
+?>
+	<link rel="stylesheet" href="/pages/admin/seo/webpage/webpage-skybox.css" />
+<? 
 	$rs = aql::select("website { where domain = '{$_SERVER['SERVER_NAME']}' }");
 	$website_id = $rs[0]['website_id'];
 	$aql="website_page { where page_path = '{$_POST['page_path']}' and website_id = {$website_id} }";
