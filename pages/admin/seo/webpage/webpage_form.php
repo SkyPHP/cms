140604d3d61a9ace1c8d900715c69a34579e172e
zgr024 <zach@joonbug.com>
2011-04-08 13:48:51 -0400

    t

--- pages/admin/seo/webpage/webpage_form.php
+++ pages/admin/seo/webpage/webpage_form.php
@@ -6,7 +6,7 @@
 			if (!isset($header)) {
 ?>
 				<fieldset style="width:180px; margin-right:15px; border: 1px solid #ccc; padding:10px; float:left;">
-                	<legend class="legend"><?=ucwords(str_replace('_',' ',$type))?></legend>
+                	<legend style="border: 1px solid #ccc; font-weight:bold; padding:2px 5px 2px 5px;"><?=ucwords(str_replace('_',' ',$type))?></legend>
 <?
 				$header = $type;
 			}
