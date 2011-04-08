3b016e65f920c3d784629ef960e25d4ab0853ba0
zgr024 <zach@joonbug.com>
2011-04-08 15:41:31 -0400

    t

--- pages/admin/seo/webpage/ajax/save-seo.php
+++ pages/admin/seo/webpage/ajax/save-seo.php
@@ -6,12 +6,13 @@
 			'mod__person_id' => PERSON_ID,
 			'update_time' => 'now()'
 		);	
-	if (isset($rs[0]['website_page_data_ide'])) {
+	if (is_numeric($rs[0]['website_page_data_id'])) {
 		$update=aql::update('website_page_data',$data,$rs[0]['website_page_data_ide']);
 		if ($update) exit('success');
 		else exit($update);
 	}
 	else {
+		$data['website_page_id']=decrypt($_POST['website_page_ide'],'website_page');
 		$insert=aql::insert('website_page_data',$data);
 		if ($insert) exit('success');
 		else exit($insert);
