30913fdaccf55a038cc299515e78bf7419f4fa7a
zgr024 <zach@joonbug.com>
2011-04-08 15:59:54 -0400

    t

--- pages/admin/seo/webpage/webpage_form.php
+++ pages/admin/seo/webpage/webpage_form.php
@@ -36,7 +36,7 @@
 <?
 				} else {
 ?>
-	    			<input type="text" class="seo-input" field="<?=$field?>" value="<?=$fields[$field]?>" size="20" />
+	    			<input type="text" class="seo-input" field="<?=$field?>" value="<?=$fields[$field]?>" size="25" />
 <?                    
 				}
 ?>				 
