fec50488691dec488d955eccae02bc8270e23d4b
Will Schmid <will123195@gmail.com>
2011-04-12 17:56:28 -0400

    cms-html5.css

--- templates/html5/html5.php
+++ templates/html5/html5.php
@@ -1,7 +1,9 @@
 <?
 
+// hook some extra js and css into the html5 template
 if ( $template_area == 'top' ) {
     $this->template_js[] = '/templates/html5/cms-html5.js';
+    $this->template_css[] = '/templates/html5/cms-html5.css';
 }
 $skyphp_codebase_path = end($GLOBALS['codebase_path_arr']);
 include( $skyphp_codebase_path . 'templates/html5/html5.php' );
