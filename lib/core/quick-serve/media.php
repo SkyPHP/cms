2a948a8c2308d44ffd931d420311a5baefb83647
Stan Rozenraukh <stan@jooonbug.com>
2011-04-14 17:20:06 -0400

    fix quick-serve

--- lib/core/quick-serve/media.php
+++ lib/core/quick-serve/media.php
@@ -2,7 +2,6 @@
 include_once( $sky_install_path . 'lib/core/functions.inc.php' );
 include_once( $sky_install_path . 'lib/adodb/adodb.inc.php' );
 @include_once( $sky_install_path . 'config.php' );
-
 $needle = '/media/';
 $start = strlen($needle);
 $end = strpos($_SERVER['REQUEST_URI'],'/', $start);
@@ -56,12 +55,15 @@ default:
 			if (!$r->EOF) {
 				$slug = $r->Fields('slug');
 				$instance = $r->Fields('instance');
-				if ($instance) $instance_folder = '/' . $instance;
+				if ($instance) $instance_folder = '/media/' . $instance;
 				$file_type = $r->Fields('file_type');
 				$vfolder_path = $r->Fields('vfolder_path');
                 $dest_dir = $sky_media_local_path . $vfolder_path . $instance_folder;
 				$local_path = $dest_dir . '/' . $slug . '.' . $file_type;
 				if ( !file_exists($local_path) ) {
+					include('lib/core/class.aql2array.php');
+					include('lib/core/class.aql.php');
+					include('lib/core/class.media.php');
                     media::get_if_not_here($r->Fields('media_item_id'),$sky_media_src_path, $file_type,$local_path,$dest_dir);
                 }
                 if (file_exists($local_path)) {
