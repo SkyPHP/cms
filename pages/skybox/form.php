367286cdd465505c69dc44f200cb86d78d3bbf13
Stan Rozenraukh <stan@jooonbug.com>
2011-04-27 11:11:58 -0400

    aql::form now does not include opening and close form tags + submit button, just fields

--- pages/skybox/form.php
+++ pages/skybox/form.php
@@ -3,5 +3,7 @@ $model = $_POST['sky_qs'][0];
 $ide = $_POST['sky_qs'][1];
 $p->title = 'Edit';
 $p->template('skybox', 'top');
-aql::form($model, $ide);
+?><form model="<?=$model?>" method="post" class="aqlForm standard_form" action="/save/v2/<?=$model?>"><?
+	aql::form($model, $ide);
+?><input type="submit" value="Save" class="button" /></form><?
 $p->template('skybox', 'bottom');
