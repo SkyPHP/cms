242e55c586ca578774b7b16b2fa80a3fab81ad35
Stan Rozenraukh <stan@jooonbug.com>
2011-06-15 16:22:39 -0400

    saveForm only binds to submit once, or overrides. Generic Form title: edit or add new

--- pages/skybox/form.php
+++ pages/skybox/form.php
@@ -1,7 +1,7 @@
 <?
 $model = $p->queryfolders[0];
 $ide = $p->queryfolders[1];
-$p->title = 'Edit';
+$p->title = ($ide) ? 'Edit' : 'Add New';
 $p->template('skybox', 'top');
 ?><form model="<?=$model?>" method="post" class="aqlForm standard_form" action="/save/v2/<?=$model?>"><?
 	aql::form($model, $ide);
