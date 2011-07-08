0ab94e9e52abd69d9492bdef769d950a4b3805ac
zach <zach@joonbug.com>
2011-07-08 18:46:02 -0400

    keyword density

--- pages/admin/seo/keyword-density/keyword-density.php
+++ pages/admin/seo/keyword-density/keyword-density.php
@@ -3,7 +3,7 @@
 	$p->template('crave','top');
 ?>
 <div><input type="text" id="word-search" style="width:200px;" /> <input type="button" id="search" value="Search"></div>
-<div><textarea id="density-area" style="width:600px; height:300px"></textarea></div>
+<div style="margin-bottom:10px;"><textarea id="density-area" style="width:600px; height:300px"></textarea></div>
 <div>Total Words (<span id="total-words">0</span>)</div>
 <div id="density"></div>
 <?
@@ -12,7 +12,6 @@
 <script language="javascript">
 	$(function() {
 		$('#density-area').live('keyup click focusout focusin change', function() {
-			search_count = 0;
 			text = jQuery.trim($(this).val()).replace(/\s+/g," ")
 			if($(this).val() == '') {
 				numWords = 0
@@ -30,14 +29,17 @@
 			else numWords = text.split(' ').length;
 			if (numWords > 0) {
 				words = text.split(' ')
+				// remove the unwanted characters
+				newtext=''				
 				$.each(words,function(index,value) {
-					check = value.toLowerCase()
-					if (check == word) search_count++
+					word = value.toLowerCase().replace("!","").replace("?","").replace(".","").replace(";","").replace(":","").replace(";","").replace(",","")
+					newtext = newtext+' '+word
 				})
-				density = (search_count / numWords) * 100
+				var count = newtext.split(word).length - 1;
+				density = (count / numWords) * 100
 				percent = density.toFixed(2)
 				
-				$('#density').html("<strong>"+word+"</strong> was found "+search_count+" times.<br> It has a density of "+percent+"%")
+				$('#density').html("<strong>"+word+"</strong> was found "+count+" times.<br> It has a density of "+percent+"%")
 			}
 			else $('#density').html('')
 		})
