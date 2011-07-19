e7ec858e06bc0145d38830c0b9c8be989980b5c2
Zach R <zach@joonbug.com>
2011-07-19 17:03:35 -0400

    seo fetch with uri capability

--- pages/run-first.php
+++ pages/run-first.php
@@ -1,10 +1,3 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?
+	include ('pages/seo.php');
+?>
---
+++ pages/seo.php
@@ -0,0 +1,50 @@
+<?
+	global $seo_field_array;
+	
+	elapsed('before website table query');
+	if (!$website_id) $rs = sql("SELECT id FROM website where domain = '".$_SERVER['SERVER_NAME']);
+	elapsed('after website table query');
+	$website_id = $rs->Fields('id');
+	if (is_array($seo_field_array)) {
+		
+		$mem_key = "seo:".$website_id.":".$p->page_path;
+		$page_data = mem($mem_key);
+		if (!$page_data) {
+			$rs = aql::select("website_page { url_specific where page_path = '".$p->page_path."' and website_id = ".$website_id."}");
+			$pd = aql::select("website_page_data { field, value where website_page_id = {$rs[0]['website_page_id'] } ");
+			foreach ($pd as $p) {
+				$page_data[$p['field']] = $p['value'];	
+			}
+			if ($rs[0]['url_specific'] == 1) $page_data['url_specific']=true;
+			mem($mem_key, $page_data);
+		}
+		
+		foreach ($page_data as $field => $value)  {
+			$p->var[$field] = $value;	
+		}
+		
+		if ($page_data['url_specific']) {
+			$mem_key = "seo:".$website_id.":".$_SERVER['PATH_INFO'];
+			$uri_data = mem($mem_key);
+			if (!$uri_data) {
+				$ud = aql::select("website_uri_data { field, value where website_id = ".$website_id." and uri = '".$_SERVER['PATH_INFO']." }");
+				foreach($ud as $u) {
+					$uri_data[$u['field']] = $u['value'];
+				}
+				mem($mem_key, $uri_data);
+			}
+			
+			foreach($uri_data as $field => $value) {
+				$p->var[$field]=$value;	
+			}
+		}
+		
+		foreach($seo_field_array as $type => $ar) {
+			foreach ($ar as $field => $max) {
+				 $val = $p->var[$field];
+				 eval('$p->var["'.$field.'"] = stripslashes("'.addslashes($val).'");'); 
+			}			
+		}
+		
+	}
+?>
