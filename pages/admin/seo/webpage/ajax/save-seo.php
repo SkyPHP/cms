bea26f410994c2c94230922a250f059c40ae74c2
Zach R <zach@joonbug.com>
2011-07-19 18:44:57 -0400

    remove the memcache of the pages data upon saving a field

--- pages/admin/seo/webpage/ajax/save-seo.php
+++ pages/admin/seo/webpage/ajax/save-seo.php
@@ -1,5 +1,10 @@
 <? 
 		$rs = aql::select("website_page_data { where website_page_id = {$_POST['wp_id']} and field = '{$_POST['field']}' }");
+		$rs2 = aql::select("website_page { page_path, website_id where website_page.id = {$_POST['wp_id']} }");
+		$website_id = $rs2[0]['website_id'];
+		$page_path = $rs2[0]['page_path'];
+		$mem_key = "seo:".$website_id.":".$page_path;
+		mem($mem_key, '');
 		$data = array(
 			'field' => $_POST['field'],
 			'value' => $_POST['value'],
