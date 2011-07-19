f3c69775bd3ab824f9d334259a06dc9839514c0e
Zach R <zach@joonbug.com>
2011-07-19 18:22:02 -0400

    if no website_page record bypass

--- pages/seo.php
+++ pages/seo.php
@@ -13,7 +13,7 @@
 		if (!$page_data) {
 			$rs = aql::select("website_page { url_specific where page_path = '".$p->page_path."' and website_id = ".$website_id."}");
 			
-			$pd = aql::select("website_page_data { field, value where website_page_id = {$rs[0]['website_page_id']} } ");
+			if ($rs) $pd = aql::select("website_page_data { field, value where website_page_id = {$rs[0]['website_page_id']} } ");
 			if (is_array($pd)) {			
 				foreach ($pd as $p) {
 					$page_data[$p['field']] = $p['value'];	
