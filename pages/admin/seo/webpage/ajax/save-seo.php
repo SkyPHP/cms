a5c30f2cafebc2ce8ce41b0687e586175490f694
zach <zach@joonbug.com>
2011-08-02 16:31:25 -0400

    uri saving

--- pages/admin/seo/webpage/ajax/save-seo.php
+++ pages/admin/seo/webpage/ajax/save-seo.php
@@ -24,21 +24,21 @@
 			else exit($insert);
 		}	
 	} else {
-		$rs = aql::select("website_uri_data { where website_page_id = {$_POST['wp_id']} and field = '{$_POST['field']}' }");
+		$rs = aql::select("website_uri_data { where uri = {$_POST['uri']} and field = '{$_POST['field']}' }");
 		$data = array(
 			'field' => $_POST['field'],
 			'value' => $_POST['value'],
 			'mod__person_id' => PERSON_ID,
 			'update_time' => 'now()'
 		);	
-		if (is_numeric($rs[0]['website_page_data_id'])) {
-			$update=aql::update('website_page_data',$data,$rs[0]['website_page_data_ide']);
+		if (is_numeric($rs[0]['website_uri_data_id'])) {
+			$update=aql::update('website_uri_data',$data,$rs[0]['website_uri_data_ide']);
 			if ($update) exit('saved');
 			else exit($update);
 		}
 		else {
-			$data['website_page_id']=$_POST['wp_id'];
-			$insert=aql::insert('website_page_data',$data);
+			$data['uri'] = $_POST['uri'];
+			$insert=aql::insert('website_uri_data',$data);
 			if ($insert) exit('saved');
 			else exit($insert);
 		}	
