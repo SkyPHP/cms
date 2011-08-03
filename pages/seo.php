e0eb3df8114d31eb7cbc4da4c5ebb949b609095b
Zach R <zach@joonbug.com>
2011-08-03 16:28:53 -0400

    URL cb

--- pages/seo.php
+++ pages/seo.php
@@ -34,7 +34,7 @@
 				$uri_data = mem($mem_key);
 				$uri_data = false;
 				if (!$uri_data) {
-					$ud = aql::select("website_uri_data { field, value where website_id = {$website_id} and uri = '{$p->urlpath}' }");
+					$ud = aql::select("website_uri_data { field, value where website_id = {$website_id} and uri = '{$p->urlpath}' and on_website = 1 and value is not null }");
 					foreach($ud as $u) {
 						$uri_data[$u['field']] = $u['value'];
 					}
