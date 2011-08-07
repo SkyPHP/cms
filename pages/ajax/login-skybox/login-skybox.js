50db5f1d1b675b764f373c63eb2705b3a445e2be
Will Schmid <will123195@gmail.com>
2011-08-07 14:07:51 -0400

    login skybox js tweaks

--- pages/ajax/login-skybox/login-skybox.js
+++ pages/ajax/login-skybox/login-skybox.js
@@ -9,8 +9,8 @@
         }
     });
     $('#login_form').live('submit',function(){
-        var button = $('#login_box').html();
-        $('#login_box').html('<img src="/images/loading.gif" />');
+        $('#login_button').val('Authenticating...').css('color','gray');
+        if ($('#login_message').is(":visible")) $('#login_message').fadeTo('fast',0.01);
         $.post(
             '/ajax/login-skybox/authenticate',
             $(this).serialize(),
@@ -20,14 +20,18 @@
                     url = removeParam('skybox',url);
                     url = removeParam('logout',url);
                     window.location.href = url;
-                } else if (data=='false') {
-                    $('#login_box').html(button);
-                    $('#login_password').val('');
-                    $('#login_message').html('Incorrect login.  Try again.');
                 } else {
+                    if (data=='false') {
+                        $('#login_message').html('Incorrect login.  Try again.');
+                    } else {
+                        $('#login_message').html(data);
+                    }
+                    if ( $('#login_message').is(':visible') ) $('#login_message').fadeTo('fast',1);
+                    else $('#login_message').slideDown('fast');
                     $('#login_password').val('');
-                    $('#login_message').html(data);
-                    $('#login_box').html(button);
+                    $('#login_button').val('Sign In').css('color','black');
+                    if ( $('#login_username').val() == '' ) $('#login_username').focus();
+                    else $('#login_password').focus();
                 }
             }
         );
