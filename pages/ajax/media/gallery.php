1aa30e33beabbf5f71ffb42da4a8b995332d0008
Zach R <zach@joonbug.com>
2011-08-11 21:18:48 -0400

    css

--- pages/ajax/media/gallery.php
+++ pages/ajax/media/gallery.php
@@ -1,6 +1,6 @@
-  <div class="gallery" style="width:<?=$_POST['image_width']?>px; ">  
+  <div class="gallery" style="width:<?=$_POST['image_width']?>px; overflow:hidden;">  
 	
-    <div class="slides" style="height:<?=$_POST['image_height']?>px;">
+    <div class="slides" style="">
 <?  
 	$vfolder = media::get_vfolder($_POST['vfolder']);
 	if ($vfolder['items']) foreach ($vfolder['items'] as $item) {
