ef6b68635636c6906fb87e1e8d6996bfb980325e
Zach R <zach@joonbug.com>
2011-08-11 21:55:44 -0400

    no css on gallery.php for test

--- pages/ajax/media/gallery.php
+++ pages/ajax/media/gallery.php
@@ -1,12 +1,12 @@
-  <div class="gallery" style="width:<?=$_POST['image_width']?>px; overflow:hidden;">  
+  <div class="gallery">  
 	
-    <div class="slides" style="width:<?=$_POST['image_width']?>px; height:<?=$_POST['image_height']?>px; overflow:hidden;">
+    <div class="slides">
 <?  
 	$vfolder = media::get_vfolder($_POST['vfolder']);
 	if ($vfolder['items']) foreach ($vfolder['items'] as $item) {
 		$img = media::get_item($item['media_item_id'],$_POST['image_width'],$_POST['image_height'],true);
 ?>
-		<div class="slide" style="width:<?=$_POST['image_width']?>px; height:<?=$_POST['image_height']?>px; float:left;"><img align="left" src="<?=$img['src']?>" width="<?=$_POST['image_width']?>" height="<?=$_POST['image_height']?>" /></div>
+		<div class="slide"><img src="<?=$img['src']?>" width="<?=$_POST['image_width']?>" height="<?=$_POST['image_height']?>" /></div>
 <?		
 	}
 	else exit('No Images');
