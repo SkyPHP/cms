ff1c2302a6bb0d2f56b17f5b897bff274c55cf82
Zach R <zach@joonbug.com>
2011-08-11 22:06:54 -0400

    undo gallery changes

--- pages/ajax/media/gallery.php
+++ pages/ajax/media/gallery.php
@@ -1,12 +1,12 @@
-  <div class="gallery">  
+  <div class="gallery" style="width:<?=$_POST['image_width']?>px; overflow:hidden;">  
 	
-    <div class="slides">
+    <div class="slides" style="width:<?=$_POST['image_width']?>px; height:<?=$_POST['image_height']?>px; overflow:hidden;">
 <?  
 	$vfolder = media::get_vfolder($_POST['vfolder']);
 	if ($vfolder['items']) foreach ($vfolder['items'] as $item) {
 		$img = media::get_item($item['media_item_id'],$_POST['image_width'],$_POST['image_height'],true);
 ?>
-		<div class="slide"><?=$img['html']?></div>
+		<div class="slide" style="float:left;"><img src="<?=$img['src']?>" width="<?=$_POST['image_width']?>" height="<?=$_POST['image_height']?>" /></div>
 <?		
 	}
 	else exit('No Images');
