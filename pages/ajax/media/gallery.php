d8e65cd0269b2015d4f20d8633832e53274dfc1d
Stan Rozenraukh <stan@stanistan.com>
2011-08-12 12:07:43 -0400

    gallery.js fix

--- pages/ajax/media/gallery.php
+++ pages/ajax/media/gallery.php
@@ -1,29 +1,27 @@
-<div>
-	<div class="gallery" style="width:<?=$_POST['image_width']?>px; overflow:hidden;">	
-    	<div class="slides" style="width:<?=$_POST['image_width']?>px; height:<?=$_POST['image_height']?>px; overflow:hidden;">
+<div class="gallery" style="width:<?=$_POST['image_width']?>px; overflow:hidden;">	
+	<div class="slides" style=" height:<?=$_POST['image_height']?>px; overflow:hidden;">
 <?  
 		$vfolder = media::get_vfolder($_POST['vfolder']);
-		if ($vfolder['items']) foreach ($vfolder['items'] as $item) {
-			$img = media::get_item($item['media_item_id'],$_POST['image_width'],$_POST['image_height'],true);
+	if ($vfolder['items']) foreach ($vfolder['items'] as $item) {
+		$img = media::get_item($item['media_item_id'],$_POST['image_width'],$_POST['image_height'],true);
 ?>
-			<div class="slide"><img src="<?=$img['src']?>" width="<?=$_POST['image_width']?>" height="<?=$_POST['image_height']?>" /></div>
+		<div class="slide"><img src="<?=$img['src']?>" width="<?=$_POST['image_width']?>" height="<?=$_POST['image_height']?>" /></div>
 <?		
-		}
-		else exit('No Images');
+	}
+	else exit('No Images');
 ?>
- 	   	</div>    
-    	<div class="gallery-menu">    
-   			<ul>
-        		<li class="fbar">&nbsp;</li>
+	   	</div>    
+	<div class="gallery-menu">    
+			<ul>
+    		<li class="fbar">&nbsp;</li>
 <?
-				foreach($vfolder['items'] as $item) {
-					$img = media::get_item($item['media_item_id'],$_POST['thumb_width'],$_POST['thumb_height'],true);
+			foreach($vfolder['items'] as $item) {
+				$img = media::get_item($item['media_item_id'],$_POST['thumb_width'],$_POST['thumb_height'],true);
 ?>		
-					<li class="menuItem"><a href=""><?=$img['html']?></a></li>
+				<li class="menuItem"><a href=""><?=$img['html']?></a></li>
 <?
-				}
+			}
 ?>
-    		</ul>	
-    	</div>    
-  	</div>    
-</div>
+		</ul>	
+	</div>    
+</div>    
