5c57cc1175d8d4f3c55ce5a5420caca19257f061
Zach R <zach@joonbug.com>
2011-08-18 16:13:28 -0400

    js test

--- pages/admin/seo/duplicate-data/duplicate-data.js
+++ pages/admin/seo/duplicate-data/duplicate-data.js
@@ -43,9 +43,7 @@ $(function() {
 	});
 	
 	$('#container').not('.filter-on').live('click',function () {
-		$('.filter-on').slideUp('fast',function() {
-			$('.filter-on').css('border-bottom', '2px solid #999').removeClass('filter-on').addClass('filter');
-		});
+		alert('clicked');
 	});
 	
 });
