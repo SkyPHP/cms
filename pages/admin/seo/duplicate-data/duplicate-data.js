43755fa7997c85243586f09466d55e98ed0a74bc
Zach R <zach@joonbug.com>
2011-08-18 17:10:08 -0400

    js fix

--- pages/admin/seo/duplicate-data/duplicate-data.js
+++ pages/admin/seo/duplicate-data/duplicate-data.js
@@ -10,7 +10,7 @@ $(function() {
 
     $("#container").mouseup(function(){ 
         if(! mouse_is_inside) $('.filter-on').slideUp('fast',function() {
-			$('.filter-on').css('border-bottom', '2px solid #999').removeClass('filter-on').addClass('filter');	
+			$('.filter-area').css('border-bottom', '2px solid #999').removeClass('filter-on').addClass('filter');	
 		});
     });
 
