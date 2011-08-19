397c19abaf33cc3b14e0696bf3f374d5f927d681
zach <zach@joonbug.com>
2011-08-19 14:09:23 -0400

    same

--- pages/admin/seo/duplicate-data/ajax/filter-listing.php
+++ pages/admin/seo/duplicate-data/ajax/filter-listing.php
@@ -31,12 +31,11 @@
     		<legend class="legend"><?=$type=='phrase'?'Phrase Part ':'Sentence #'?>1 (<?=$count?> Phrases)</legend>
 <?
 			$x=0;
-			$SQL = aql::sql("dup_phrase_data { id as phrase_id, lower(phrase) as lower_phrase, phrase, volume where market != '' and base != '' and volume > 0 {$where} order by volume DESC, phrase asc }");
 			if ($listing) foreach($listing as $data) {
 				
 				$x++;
 				if ($x == 1) print_pre($data);
-				if ($x == 1) echo $SQL;
+				if ($x == 1) echo aql::sql("dup_phrase_data { id as phrase_id, lower(phrase) as lower_phrase, phrase, volume where market != '' and base != '' and volume > 0 {$where} order by volume DESC, phrase asc }");
 ?>
 				<div style="width:50px; float:left; margin-right:5px">(<?=$data['volume']?>)</div><div style="float:left;"> <input type="radio" name="phrase1" part="1" phrase="<?=$data['phrase']?>" phrase_id="<?=$data['phrase_id']?>" class="listing_radio" id="<?=$data['lower_phrase']?>" /> <label for="<?=$data['lower_phrase']?>"><?=$data['lower_phrase']?></label></div>
                 <div class="clear"></div>
