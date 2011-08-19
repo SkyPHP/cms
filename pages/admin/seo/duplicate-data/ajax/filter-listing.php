1d43d2caf287941bc8a635cfde9d973af67c47b4
zach <zach@joonbug.com>
2011-08-19 14:07:52 -0400

    aql::sql

--- pages/admin/seo/duplicate-data/ajax/filter-listing.php
+++ pages/admin/seo/duplicate-data/ajax/filter-listing.php
@@ -21,6 +21,7 @@
 			}
 			//echo $where;
 			$listing = aql::select("dup_phrase_data { id as phrase_id, lower(phrase) as lower_phrase, phrase, volume where market != '' and base != '' and volume > 0 {$where} order by volume DESC, phrase asc }");
+			$SQL = aql::sql("dup_phrase_data { id as phrase_id, lower(phrase) as lower_phrase, phrase, volume where market != '' and base != '' and volume > 0 {$where} order by volume DESC, phrase asc }");
 		}
 	}
 	$count = count($listing);
@@ -34,6 +35,7 @@
 			if ($listing) foreach($listing as $data) {
 				$x++;
 				if ($x == 1) print_pre($data);
+				if ($x == 1) echo $SQL;
 ?>
 				<div style="width:50px; float:left; margin-right:5px">(<?=$data['volume']?>)</div><div style="float:left;"> <input type="radio" name="phrase1" part="1" phrase="<?=$data['phrase']?>" phrase_id="<?=$data['phrase_id']?>" class="listing_radio" id="<?=$data['lower_phrase']?>" /> <label for="<?=$data['lower_phrase']?>"><?=$data['lower_phrase']?></label></div>
                 <div class="clear"></div>
