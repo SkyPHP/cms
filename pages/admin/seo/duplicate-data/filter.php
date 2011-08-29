a2d131c134d6126a378d803a5f4c5d1528672da7
zach <zach@joonbug.com>
2011-08-29 15:47:58 -0400

    styling of clear radio

--- pages/admin/seo/duplicate-data/filter.php
+++ pages/admin/seo/duplicate-data/filter.php
@@ -4,7 +4,9 @@
 	else $DESC = '';
 	$rs = sql("SELECT DISTINCT ".$filter." FROM ".$table." where market != '' and base != '' and volume > 0 and active = 1 order by ".$filter." ".$DESC);
 ?>
-	<input type="radio" value="" id="all_<?=$filter?>" name="<?=$filter?>" class="phrase-filter-radio" /> <label style="cursor:pointer;" for="all_<?=$filter?>">clear <?=$filter?></label>
+	<div style="margin-bottom:5px;">
+		<input type="radio" value="" id="all_<?=$filter?>" name="<?=$filter?>" class="phrase-filter-radio" /> <label style="cursor:pointer;" for="all_<?=$filter?>">clear <?=$filter?></label>
+    </div>
 <?
 	if($rs) while (!$rs->EOF) {
 ?>
