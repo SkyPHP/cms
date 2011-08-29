f19944d2d88668d9be2feee7f717df7481f48cf2
zach <zach@joonbug.com>
2011-08-29 16:35:33 -0400

    listing 2 attr's

--- pages/admin/seo/duplicate-data/duplicate-data.js
+++ pages/admin/seo/duplicate-data/duplicate-data.js
@@ -95,7 +95,7 @@ $(function() {
 				'phrase4__dup_phrase_data_id' : p4,
 				'mod__person_id' : person_id
 			};
-			$('#saved-message').aqlSave('dup_phrase',data);
+			$('#saved-message').aqlSave('dup_phrase_group',data);
 		}
 		else alert('Please select your choices from the lists below.');
 	});
