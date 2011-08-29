d8343f9831e13555b827fcb5f9332eb355604168
zach <zach@joonbug.com>
2011-08-29 17:58:00 -0400

    no cross linking

--- pages/admin/seo/duplicate-data/ajax/listing.php
+++ pages/admin/seo/duplicate-data/ajax/listing.php
@@ -13,6 +13,7 @@
 			if ($_POST['base']) $where[] = "base = '{$_POST['base']}'";
 			$type = 'phrase';
 			$where[] = "volume > 0";
+			$where[] = "market != 'none'";
 			$width = '25%';
 			$listing = aql::select("dup_phrase_data { id as phrase_id, lower(phrase) as lower_phrase, phrase, volume order by volume DESC, phrase asc }", array('dup_phrase_data'=>array('where'=>$where)));
 		}
