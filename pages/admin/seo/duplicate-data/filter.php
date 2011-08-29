22aecf31de656175af3e02e0647270108b071ab1
zach <zach@joonbug.com>
2011-08-29 15:51:59 -0400

    css

--- pages/admin/seo/duplicate-data/filter.php
+++ pages/admin/seo/duplicate-data/filter.php
@@ -5,7 +5,7 @@
 	$rs = sql("SELECT DISTINCT ".$filter." FROM ".$table." where market != '' and base != '' and volume > 0 and active = 1 order by ".$filter." ".$DESC);
 ?>
 	<div style="margin-bottom:5px;">
-		<input type="radio" value="" id="all_<?=$filter?>" name="<?=$filter?>" class="phrase-filter-radio" /> <label style="cursor:pointer;" for="all_<?=$filter?>">ALL</label>
+		<input type="radio" value="" id="all_<?=$filter?>" name="<?=$filter?>" class="phrase-filter-radio" style="margin-left:2px;" /> <label style="cursor:pointer;" for="all_<?=$filter?>">ALL</label>
     </div>
 <?
 	if($rs) while (!$rs->EOF) {
