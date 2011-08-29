053cfaae6583f685cb6062d7239854298295c7c7
zach <zach@joonbug.com>
2011-08-29 15:52:28 -0400

    css

--- pages/admin/seo/duplicate-data/filter.php
+++ pages/admin/seo/duplicate-data/filter.php
@@ -5,13 +5,13 @@
 	$rs = sql("SELECT DISTINCT ".$filter." FROM ".$table." where market != '' and base != '' and volume > 0 and active = 1 order by ".$filter." ".$DESC);
 ?>
 	<div style="margin-bottom:5px;">
-		<input type="radio" value="" id="all_<?=$filter?>" name="<?=$filter?>" class="phrase-filter-radio" style="margin-left:2px;" /> <label style="cursor:pointer;" for="all_<?=$filter?>">ALL</label>
+		<input type="radio" value="" id="all_<?=$filter?>" name="<?=$filter?>" class="phrase-filter-radio" style="margin-left:3px;" /> <label style="cursor:pointer;" for="all_<?=$filter?>">ALL</label>
     </div>
 <?
 	if($rs) while (!$rs->EOF) {
 ?>
 		<div style="margin-bottom:5px;">
-		<input type="radio" value="<?=$rs->Fields($filter)?>" id="<?=$rs->Fields($filter)?>" name="<?=$filter?>" class="phrase-filter-radio" /> <label style="cursor:pointer;" for="<?=$rs->Fields($filter)?>"><?=strtolower($rs->Fields($filter))?></label>
+		<input type="radio" value="<?=$rs->Fields($filter)?>" id="<?=$rs->Fields($filter)?>" name="<?=$filter?>" style="margin-left:3px;" class="phrase-filter-radio" /> <label style="cursor:pointer;" for="<?=$rs->Fields($filter)?>"><?=strtolower($rs->Fields($filter))?></label>
        	</div>
 <?		
 		$rs->MoveNext();
