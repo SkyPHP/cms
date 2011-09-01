7c42941a36faf2d89e661015580c977c4174bc3d
zach <zach@joonbug.com>
2011-09-01 13:36:23 -0400

    no trim

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -7,7 +7,7 @@
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-bottom:20px;">
     	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
-        	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=trim($sentence)?>" />
+        	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=$sentence?>" />
         </div>
         <div style="float:left;">
         	<input type="text" style="width:16px; text-align:right;" maxlength="2" value="<?=$key+1?>" />
