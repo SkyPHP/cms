b91df76dfd24c1ce7c1175743543e35c158ad41e
zach <zach@joonbug.com>
2011-09-01 18:05:28 -0400

    hide button

--- pages/admin/seo/duplicate-data/split-paragraph.php
+++ pages/admin/seo/duplicate-data/split-paragraph.php
@@ -5,8 +5,10 @@
 <h1><?=$p->title?></h1>
 <div class="hideable"><textarea id="paragraph" style="width:1000px; height: 100px;"></textarea></div>
 <div class="has-floats">
-	<div class="hideable"><input type="button" value="Split" id="split" /></div>
-    <div style="font-size:10px" id="hide-show">HIDE -</div>
+	<div class="hideable" style="float:left"><input type="button" value="Split" id="split" /></div>
+    <div style="float:left">
+    	<button class="hide" style="cursor:pointer; font-size:10px; border:none; background:none; margin:0; padding:0;">HIDE -</button>
+    </div>
 </div>
 <div id="results"></div>
 <?
