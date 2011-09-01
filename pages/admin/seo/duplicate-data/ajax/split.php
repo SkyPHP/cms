b09b3f503902220e1e099a1f4683393d23434b2e
zach <zach@joonbug.com>
2011-09-01 17:34:42 -0400

    css

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -4,14 +4,14 @@
 	else $man = 'checked="checked"';
 	preg_match_all('~.*?[?.!]~s',$val,$sentences);
 ?>
-	<div class="has-floats">
+	<div class="has-floats" style="margin-top: 20px;">
 		<div style="float:left; width: 900px; margin-right: 10px; font-weight:bold;">Sentences</div>
     	<div style="float:left; font-weight:bold;">Order <input type="radio" name="auto-switch" value="man" <?=$man?>> Manual&nbsp;&nbsp;&nbsp;<input type="radio" name="auto-switch" value="auto"> Auto</div>
 	</div>
 <?
 	foreach ($sentences[0] as $key => $sentence) {
 ?>	
-	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-top:20px;">
+	<div id="sentence<?=$key?>-container" class="has-floats" style="margin:10px 0 20px 0;">
     	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
         	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=trim($sentence)?>" />
