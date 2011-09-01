71f5066d4da56076b99d1ecdf30a27fa057a7448
zach <zach@joonbug.com>
2011-09-01 13:10:24 -0400

    key+1

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -5,12 +5,12 @@
 		if (trim($sentence)) {
 ?>
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-bottom:20px;">
-    	<div>Sentence <?=$key?></div>
+    	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
         	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=trim($sentence)?>." />
         </div>
         <div style="float:left;">
-        	<input type="text" size="2" maxlength="2" value="<?=$key?>" />
+        	<input type="text" size="2" maxlength="2" value="<?=$key+1?>" />
         </div>
     </div>
 <?		
