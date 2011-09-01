71b718ca322161402d858237f9686644cba9323e
zach <zach@joonbug.com>
2011-09-01 13:02:48 -0400

    js

--- pages/admin/seo/duplicate-data/split-paragraph.js
+++ pages/admin/seo/duplicate-data/split-paragraph.js
@@ -2,7 +2,6 @@ $(function() {
 	$("#split").live('click',function() {
 		var val = $('#paragraph').val()
 		if (val) {
-			
 			$.post('/admin/seo/duplicate-data/ajax/split.php',{ val: val }, function(data) {
 				$('#result').html(data);
 			});
