9a1b185097e90e05f0246b9847b5c3e3b28cc9d1
zach <zach@joonbug.com>
2011-09-01 13:06:19 -0400

    css

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -5,7 +5,7 @@
 ?>
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-bottom:20px;">
 		<div style="float:left; margin-right:10px;">
-        	<input type="text" id="sentence<?=$key?>" style="width:60%" value="<?=trim($sentence)?>." />
+        	<input type="text" id="sentence<?=$key?>" style="width:600px" value="<?=trim($sentence)?>." />
         </div>
         <div style="float:left;">
         	<input type="text" size="2" maxlength="2" value="<?=$key?>" />
