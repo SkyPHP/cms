1a0764c491a7c618eaae6d5f3ac619b3bd740b97
zach <zach@joonbug.com>
2011-09-01 17:24:36 -0400

    h1

--- pages/admin/seo/duplicate-data/split-paragraph.php
+++ pages/admin/seo/duplicate-data/split-paragraph.php
@@ -2,6 +2,7 @@
 	$p->title = "Paragraph Splitter";
 	$p->template('seo','top');
 ?>
+<h1><?=$p->title?></h1>
 <textarea id="paragraph" style="width:90%; height: 100px;"></textarea><br>
 <input type="button" value="Split" id="split" />
 <div id="results"></div>
