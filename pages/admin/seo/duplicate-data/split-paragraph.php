8b70d41a06e18065cdb73c538f244ba00d115bc8
zach <zach@joonbug.com>
2011-09-01 18:01:25 -0400

    hide the paragraph after split

--- pages/admin/seo/duplicate-data/split-paragraph.php
+++ pages/admin/seo/duplicate-data/split-paragraph.php
@@ -3,8 +3,11 @@
 	$p->template('seo','top');
 ?>
 <h1><?=$p->title?></h1>
-<textarea id="paragraph" style="width:1000px; height: 100px;"></textarea><br>
-<input type="button" value="Split" id="split" />
+<div class="hideable"><textarea id="paragraph" style="width:1000px; height: 100px;"></textarea></div>
+<div class="has-floats">
+	<div class="hideable"><input type="button" value="Split" id="split" /></div>
+    <div style="font-size:10px" id="hide-show">HIDE -</div>
+</div>
 <div id="results"></div>
 <?
 	$p->template('seo','bottom');
