3f057bdd8ece92d76d02fcf7a649349b997b111e
zach <zach@joonbug.com>
2011-09-01 13:37:38 -0400

    css

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -1,10 +1,9 @@
 <?
 	$val = $_POST['val'];
 	preg_match_all('~.*?[?.!]~s',$val,$sentences);
-	print_a($sentences);
 	foreach ($sentences[0] as $key => $sentence) {
 ?>
-	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-bottom:20px;">
+	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-top:20px;">
     	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
         	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=$sentence?>" />
