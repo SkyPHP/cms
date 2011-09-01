891d958ed151e71f5a7364493495d10b167c83b7
zach <zach@joonbug.com>
2011-09-01 13:43:08 -0400

    trim back on

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -6,7 +6,7 @@
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-top:20px;">
     	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
-        	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=$sentence?>" />
+        	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=trim($sentence)?>" />
         </div>
         <div style="float:left;">
         	<input type="text" style="width:16px; text-align:right;" maxlength="2" value="<?=$key+1?>" />
