b997bdcc522c6074df1d469657917441fe4c396e
zach <zach@joonbug.com>
2011-09-01 13:34:18 -0400

    preg_match_all

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -1,13 +1,13 @@
 <?
 	$val = $_POST['val'];
-	$sentences = explode('.',$val);
+	preg_match_all('~.*?[?.!]~s',$val,$sentences);
 	foreach ($sentences as $key => $sentence) {
 		if (trim($sentence)) {
 ?>
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-bottom:20px;">
     	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
-        	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=trim($sentence)?>." />
+        	<input type="text" id="sentence<?=$key?>" style="width:900px" value="<?=trim($sentence)?>" />
         </div>
         <div style="float:left;">
         	<input type="text" style="width:16px; text-align:right;" maxlength="2" value="<?=$key+1?>" />
