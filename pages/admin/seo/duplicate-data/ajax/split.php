15d7b83dfe3351a56bb3cbf75a65950cef238239
zach <zach@joonbug.com>
2011-09-01 17:31:41 -0400

    moved outside loop

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -1,12 +1,14 @@
 <?
 	$val = $_POST['val'];
 	preg_match_all('~.*?[?.!]~s',$val,$sentences);
-	foreach ($sentences[0] as $key => $sentence) {
-?>	
+?>
 	<div class="has-floats">
 		<div style="float:left; width: 900px; margin-right: 10px; font-weight:bold;">Sentences</div>
-        <div style="float:left; font-weight:bold;">Order <input type="radio" name="auto-switch" value="man"> Manual&nbsp;&nbsp;&nbsp;<input type="radio" name="auto-switch" value="auto"> Auto</div>
+    	<div style="float:left; font-weight:bold;">Order <input type="radio" name="auto-switch" value="man"> Manual&nbsp;&nbsp;&nbsp;<input type="radio" name="auto-switch" value="auto"> Auto</div>
 	</div>
+<?
+	foreach ($sentences[0] as $key => $sentence) {
+?>	
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-top:20px;">
     	<div>Sentence <?=$key+1?></div>
 		<div style="float:left; margin-right:10px;">
