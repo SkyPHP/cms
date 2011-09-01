a26e01278c6cd6fe86c6a541778e51d79fd16f30
zach <zach@joonbug.com>
2011-09-01 13:35:39 -0400

    check the array

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -1,8 +1,8 @@
 <?
 	$val = $_POST['val'];
 	preg_match_all('~.*?[?.!]~s',$val,$sentences);
+	print_a($sentences);
 	foreach ($sentences as $key => $sentence) {
-		if (trim($sentence)) {
 ?>
 	<div id="sentence<?=$key?>-container" class="has-floats" style="margin-bottom:20px;">
     	<div>Sentence <?=$key+1?></div>
@@ -14,6 +14,5 @@
         </div>
     </div>
 <?		
-		}
 	}
 ?>
