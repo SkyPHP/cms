905aeb7680d2ea0908214cb73d5dd56bee7f7b21
zach <zach@joonbug.com>
2011-09-01 17:33:40 -0400

    checked box on start is manual

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -1,10 +1,12 @@
 <?
 	$val = $_POST['val'];
+	if ($_POST['auto']) $auto = 'checked="checked"';
+	else $man = 'checked="checked"';
 	preg_match_all('~.*?[?.!]~s',$val,$sentences);
 ?>
 	<div class="has-floats">
 		<div style="float:left; width: 900px; margin-right: 10px; font-weight:bold;">Sentences</div>
-    	<div style="float:left; font-weight:bold;">Order <input type="radio" name="auto-switch" value="man"> Manual&nbsp;&nbsp;&nbsp;<input type="radio" name="auto-switch" value="auto"> Auto</div>
+    	<div style="float:left; font-weight:bold;">Order <input type="radio" name="auto-switch" value="man" <?=$man?>> Manual&nbsp;&nbsp;&nbsp;<input type="radio" name="auto-switch" value="auto"> Auto</div>
 	</div>
 <?
 	foreach ($sentences[0] as $key => $sentence) {
