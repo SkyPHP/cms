6df3588a3af05f7a9b6e61e89eec58a5fbddabf0
zach <zach@joonbug.com>
2011-09-01 17:50:08 -0400

    css

--- pages/admin/seo/duplicate-data/ajax/split.php
+++ pages/admin/seo/duplicate-data/ajax/split.php
@@ -6,7 +6,7 @@
 ?>
 	<div class="has-floats" style="margin-top: 20px;">
 		<div style="float:left; width: 900px; margin-right: 10px; font-weight:bold;">Sentences</div>
-    	<div style="float:left; font-weight:bold;">\
+    	<div style="float:left; font-weight:bold;">
         	Order<br>
         	<input type="radio" name="auto-switch" value="man" <?=$man?>> Manual<br>
         	<input type="radio" name="auto-switch" value="auto"> Auto
