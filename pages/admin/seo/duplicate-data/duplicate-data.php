c116f088def62d5381c23da7b01f1d1e280b3845
zach <zach@joonbug.com>
2011-09-03 16:36:30 -0400

    concatenation

--- pages/admin/seo/duplicate-data/duplicate-data.php
+++ pages/admin/seo/duplicate-data/duplicate-data.php
@@ -72,30 +72,32 @@
      	<input type="radio" id="auto-switch-off" <? if ($_GET['area'] != 'auto') echo 'checked' ?> value="manual" class="a-or-m-switch" name="auto-switch" /> <label for="auto-switch-off">Single</label><br>
         <input type="radio" id="auto-switch-on" <? if ($_GET['area'] == 'auto') echo 'checked' ?> value="auto" class="a-or-m-switch" name="auto-switch" /> <label for="auto-switch-on">Multi</label><br>
   	</div>
-	<div id="auto" style="display:none;">
+	<div style="padding-top:10px;">
+		<div style="float:left; margin-right:15px; font-weight:bold;">Filters:</div>
+		<input type="hidden" id="table" value="<?=$table?>" />
+		<input type="hidden" id="char_count_limit" value="<?=$char_count_limit?>" />
+		<input type="hidden" id="seo_field" value="<?=str_replace('-','_',IDE)?>" />
 <?
-		include ('pages/admin/seo/duplicate-data/ajax/multi.php');
+		foreach ($filters as $filter) {
+?>			<div style="float:left; margin-right:40px;">
+				<div class="filter" type="<?=$type?>" style="font-weight:bold; width: 175px; padding-left:5px; cursor:pointer; border: 1px solid #999; border-bottom: 2px solid #999;" filter="<?=$filter?>"><?=str_replace('_',' ',$filter)?><span id="<?=$filter?>_selected" style="text-transform:lowercase"></span></div>
+				<div id="<?=$filter?>" style="position:absolute; display:none; min-width:180px; background-color: #fff; border-bottom: 1px solid #999; border-left: 1px solid #999; border-right: 1px solid #999;" class="filter-area"><? include('pages/admin/seo/duplicate-data/filter.php') ?></div>
+			</div>
+<?	
+		}
 ?>
+		<div class="clear"></div>
+	</div>
+	
+	<div id="auto" style="display:none;">
+		<div id="multi-listing">
+<?
+			include ('pages/admin/seo/duplicate-data/ajax/multi-listing.php');
+?>
+		</div>
     </div>
 	<div id="manual" <? if ($_GET['area'] == 'auto') echo 'style="display:none;"'; else echo 'class="a-or-m-on"'; ?>>
     	
-		<div style="padding-top:10px;">
-			<div style="float:left; margin-right:15px; font-weight:bold;">Filters:</div>
-			<input type="hidden" id="table" value="<?=$table?>" />
-			<input type="hidden" id="char_count_limit" value="<?=$char_count_limit?>" />
-			<input type="hidden" id="seo_field" value="<?=str_replace('-','_',IDE)?>" />
-	<?
-			foreach ($filters as $filter) {
-	?>			<div style="float:left; margin-right:40px;">
-					<div class="filter" type="<?=$type?>" style="font-weight:bold; width: 175px; padding-left:5px; cursor:pointer; border: 1px solid #999; border-bottom: 2px solid #999;" filter="<?=$filter?>"><?=str_replace('_',' ',$filter)?><span id="<?=$filter?>_selected" style="text-transform:lowercase"></span></div>
-					<div id="<?=$filter?>" style="position:absolute; display:none; min-width:180px; background-color: #fff; border-bottom: 1px solid #999; border-left: 1px solid #999; border-right: 1px solid #999;" class="filter-area"><? include('pages/admin/seo/duplicate-data/filter.php') ?></div>
-				</div>
-	<?	
-			}
-	?>
-			<div class="clear"></div>
-		</div>
- 
 		<fieldset style="width:85%">
 			<legend class="legend">Final Phrase</legend>
 			<div id="saved-message"></div>
