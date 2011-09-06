d9f8c0880f8420aa60e83a79a60f1bb526973ba7
Zach R <zach@joonbug.com>
2011-09-06 16:44:00 -0400

    js error fix

--- pages/admin/seo/duplicate-data/duplicate-data.js
+++ pages/admin/seo/duplicate-data/duplicate-data.js
@@ -173,7 +173,7 @@ $(function() {
 		trigger_count();
 	});
 	
-	$('input[name=multi-switch').live('change',function() {
+	$('input[name=multi-switch]').live('change',function() {
 		val = $(this).val();
 		if (val='multi') {
 			$('#single').fadeOut('slow',function() {
