8c07576b6d20ba7d44993c817cb699e1fc2a8437
zach <zach@joonbug.com>
2011-09-07 16:08:55 -0400

    order didn't save

--- pages/admin/seo/duplicate-data/ajax/save-auto-sentences.php
+++ pages/admin/seo/duplicate-data/ajax/save-auto-sentences.php
@@ -4,12 +4,13 @@
 		$rs = aql::select("dup_sentence_group { group_number order by group_number desc limit 1 }");
 		$num = $rs[0]['group_number'] + 1;
 		foreach($_POST['list'] as $list) {
-			$list2 = explode(',',$list);
-			foreach($list2 as $id) {
+			$ids = explode(',',$list);
+			foreach($ids as $id) {
 				$data = array(
 					'dup_sentence_data_id' => $id,
 					'group_number' => $num,
-					'order'=>$list
+					'order' => $list,
+					'mod__person_id' => PERSON_ID
 				);
 				aql::insert("dup_sentence_group",$data);
 			}
