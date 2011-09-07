da8a9a00867b4a15e66c7f7eab40b8ead5030436
zach <zach@joonbug.com>
2011-09-07 16:07:00 -0400

    store the sentence order

--- pages/admin/seo/duplicate-data/ajax/save-auto-sentences.php
+++ pages/admin/seo/duplicate-data/ajax/save-auto-sentences.php
@@ -4,11 +4,12 @@
 		$rs = aql::select("dup_sentence_group { group_number order by group_number desc limit 1 }");
 		$num = $rs[0]['group_number'] + 1;
 		foreach($_POST['list'] as $list) {
-			$list = explode(',',$list);
-			foreach($list as $id) {
+			$list2 = explode(',',$list);
+			foreach($list2 as $id) {
 				$data = array(
 					'dup_sentence_data_id' => $id,
-					'group_number' => $num
+					'group_number' => $num,
+					'order'=>$list
 				);
 				aql::insert("dup_sentence_group",$data);
 			}
