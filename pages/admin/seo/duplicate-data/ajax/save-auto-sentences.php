25b070020099462e7058cfa8bb9cdab2c9c9498d
zach <zach@joonbug.com>
2011-09-07 16:19:36 -0400

    aql error

--- pages/admin/seo/duplicate-data/ajax/save-auto-sentences.php
+++ pages/admin/seo/duplicate-data/ajax/save-auto-sentences.php
@@ -1,8 +1,6 @@
 <?
 	if (!$_POST['list']) exit('Please Select a Paragraph');
 	else {
-		$rs = aql::select("dup_sentence_group { group_number order by group_number desc limit 1 }");
-		$num = $rs[0]['group_number'] + 1;
 		foreach($_POST['list'] as $list) {
 			$data = array(
 				'order' => $list,
