76df16dc642a8f12d43e67cd06585bac04d597fd
Josh Hassara <josh.hassara@joonbug.com>
2011-09-09 15:42:15 -0400

    password changes

--- pages/reset-password/includes/set_password_form.php
+++ pages/reset-password/includes/set_password_form.php
@@ -6,14 +6,14 @@ $o->_token = $o->getToken();
 
 <div class="info" id="response_div">Enter your new password</div> 
 <form model="person" class="aqlForm">
-	
+    <input type="hidden" name="_token" value="<?=$o->_token?>" />
+    <input type="hidden" name="person_ide" value="<?=$o->person_ide?>" />	
+    
     <label for="password1">New password</label>
 	<input name="password1" id="password1" type="password" style="width:200px;" />
 	
     <label for="password2">Repeat new password</label>
     <input name="password2" id="password2" type="password" style="width:200px;" />
-
-    <input name="_token" type="hidden" value="<?=$o->_token?>" />
 
     <input type="submit" value="Submit" style="width:75px;margin-top:25px;" />
 
