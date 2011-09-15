b163e8311d7e4fe6cc35ffb734450df643b4d7dc
Will Schmid <will123195@gmail.com>
2011-09-15 17:07:40 -0400

    fix array walk

--- lib/class/class.vf.php
+++ lib/class/class.vf.php
@@ -3,7 +3,7 @@
 include('lib/class/class.vfolder_client.php');
 
 array_walk(vf::$deps, function($dep) {
-   if (!class_exists($dep)) include 'lib/class/class.'.$dep.'.php';
+   if (!class_exists($dep)) include 'lib/vfolder/class.'.$dep.'.php';
 });
 
 class vf {
