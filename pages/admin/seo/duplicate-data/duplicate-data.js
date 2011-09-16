04414dfc668c43aee8f2369e1b6cd87553d64849
zach <zach@joonbug.com>
2011-09-16 15:36:09 -0400

    js

--- pages/admin/seo/duplicate-data/duplicate-data.js
+++ pages/admin/seo/duplicate-data/duplicate-data.js
@@ -99,11 +99,11 @@ $(function() {
 		phrase_id = $(this).attr('phrase_id');
 		if ($(this).attr('checked')) {
 			$('#listing2_'+phrase_id).hide();
-			$('.save').attr('visibility','');
+			$('.save').css('visibility','visible');
 		}
 		else {
 			$('#listing2_'+phrase_id).show();
-			$('.save').attr('visibility','hidden');		
+			$('.save').css('visibility','hidden');		
 		}
 	});
 	
