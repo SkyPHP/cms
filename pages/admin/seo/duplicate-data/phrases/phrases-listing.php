d450c44fed7b43a9e9dcf074c9523b7af124f3f1
zach <zach@joonbug.com>
2011-09-17 12:03:06 -0400

    edit button complete, maybe use sybox instead of profile

--- pages/admin/seo/duplicate-data/phrases/phrases-listing.php
+++ pages/admin/seo/duplicate-data/phrases/phrases-listing.php
@@ -20,7 +20,6 @@
 		foreach($phrases as $phrase_id) {
 			$x++;
 			$o = new dup_phrase_data($phrase_id);	
-			krumo ($o->dataToArray());
 ?>
 			<tr class="<?=$x%2?'alternate':'row'?>">
 				<td class="column" valign="middle"><?=$o['phrase']?></td>
@@ -29,7 +28,7 @@
 				<td class="column" valign="middle"><?=$o['keyword']?></td>
 				<td class="column" valign="middle"><?=$o['holiday']?></td>
 				<td class="column" valign="middle"><?=$o['base']?></td>
-				<td class="column" valign="middle" style="text-align:center;"><input class="edit" type="button" phrase_ide="<?=$o['dup_phrase_ide']?>" value="edit"></td>		
+				<td class="column" valign="middle" style="text-align:center;"><input class="edit" type="button" phrase_ide="<?=$o['dup_phrase_data_ide']?>" value="edit"></td>		
 			</tr>
 <?
 		}
