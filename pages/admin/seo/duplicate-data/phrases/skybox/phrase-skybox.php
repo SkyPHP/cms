2a62c59dff8c11ad56f9d2a743ba45cb18ce93c3
zach <zach@joonbug.com>
2011-09-17 13:08:13 -0400

    skybox

--- pages/admin/seo/duplicate-data/phrases/skybox/phrase-skybox.php
+++ pages/admin/seo/duplicate-data/phrases/skybox/phrase-skybox.php
@@ -6,32 +6,34 @@
 		<div class="field">
 			<? $field = "phrase" ?>
 			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
-			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+			<input style="width:250px;" type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
-		<div class="field">
+		<div class="field float-left" style="margin-right:20px;">
 			<? $field = "category" ?>
 			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
-			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+			<input style="width:100px;" type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
-		<div class="field">
+		<div class="field float-left">
 			<? $field = "sub_category" ?>
 			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
-			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+			<input style="width:100px;" type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
-		<div class="field">
+		<div class="clear"></div>
+		<div class="field float-left" style="margin-right:20px;">
 			<? $field = "volume" ?>
 			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
-			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+			<input style="width:100px;" type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
-		<div class="field">
+		<div class="field float-left">
 			<? $field = "holiday" ?>
 			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
-			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+			<input style="width:100px;" type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
+		<div class="clear"></div>
 		<div class="field">
 			<? $field = "keyword" ?>
 			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
-			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+			<input style="width:100px;" type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>		
 <?	
 	$p->template('skybox','bottom');
