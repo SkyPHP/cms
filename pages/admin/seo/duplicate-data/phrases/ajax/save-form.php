3429124292b21935b94b954371757b581e26f4e8
zach <zach@joonbug.com>
2011-09-17 13:59:00 -0400

    no token error message

--- pages/admin/seo/duplicate-data/phrases/ajax/save-form.php
+++ pages/admin/seo/duplicate-data/phrases/ajax/save-form.php
@@ -1,5 +1,5 @@
 <?
-	if (!$_POST['_token']) exit('No Token Given');
+	if (!$_POST['_token']) print_a($_POST);
 	else unset($_POST['_token']);
 	if ($_POST['dup_phrase_data_ide']) {
 		$ide = $_POST['dup_phrase_data_ide'];
