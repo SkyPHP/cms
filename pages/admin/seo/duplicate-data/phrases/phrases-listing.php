f7f02fbd1b071660ee6316bdd318a422cb883782
zach <zach@joonbug.com>
2011-09-17 11:25:32 -0400

    test

--- pages/admin/seo/duplicate-data/phrases/phrases-listing.php
+++ pages/admin/seo/duplicate-data/phrases/phrases-listing.php
@@ -6,6 +6,7 @@
 		$phrases = dup_phrase_data::getList($a);
 		
 		foreach($phrases as $phrase_id) {
+			echo $phrase_id;
 			$dup_phrase = new dup_phrase_data($phrase_id);	
 			krumo($dup_phrase);
 		}
