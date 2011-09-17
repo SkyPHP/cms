a001e46cefb4552263fe8e7c783f26a211e95ad0
zach <zach@joonbug.com>
2011-09-17 11:48:50 -0400

    css

--- pages/admin/seo/duplicate-data/phrases/phrases-listing.php
+++ pages/admin/seo/duplicate-data/phrases/phrases-listing.php
@@ -5,7 +5,7 @@
 		$a = array();
 		$phrases = dup_phrase_data::getList($a);
 ?>
-	<table width="1500px" cellpadding="5">
+	<table width="1500px">
 		<tr class="header">
 			<th class="title">Phrase</th>
 			<th class="title">Category</th>
