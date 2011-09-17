cd9d49547054e2cee269eaac63e2645d57f818ba
zach <zach@joonbug.com>
2011-09-17 12:50:48 -0400

    js

--- pages/admin/seo/duplicate-data/phrases/phrases.js
+++ pages/admin/seo/duplicate-data/phrases/phrases.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $(function() {
 	$('.edit').live('click',function() {
-		$.skybox('/admin/seo/duplicate-data/phrases/skybox/phrase-skybox'+$(this).attr('phrase_ide'));
+		$.skybox('/admin/seo/duplicate-data/phrases/skybox/phrase-skybox/'+$(this).attr('phrase_ide'));
 	});
 });
