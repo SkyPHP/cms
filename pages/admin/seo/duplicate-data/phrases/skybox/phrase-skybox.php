77392e3344088ca4f530a3dd875ed2ca85d04524
zach <zach@joonbug.com>
2011-09-17 12:58:18 -0400

    skybox form

--- pages/admin/seo/duplicate-data/phrases/skybox/phrase-skybox.php
+++ pages/admin/seo/duplicate-data/phrases/skybox/phrase-skybox.php
@@ -2,7 +2,37 @@
 	$o = new dup_phrase_data(IDE);
 	$p->title = "Phrase Skybox (".$o['dup_phrase_data_id'].")";
 	$p->template('skybox','top');
-	
-	krumo ($o);
+?>
+		<div class="field">
+			<? $field = "phrase" ?>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+		</div>
+		<div class="field">
+			<? $field = "category" ?>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+		</div>
+		<div class="field">
+			<? $field = "sub_category" ?>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+		</div>
+		<div class="field">
+			<? $field = "volume" ?>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+		</div>
+		<div class="field">
+			<? $field = "holiday" ?>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+		</div>
+		<div class="field">
+			<? $field = "keyword" ?>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
+		</div>		
+<?	
 	$p->template('skybox','bottom');
 ?>
