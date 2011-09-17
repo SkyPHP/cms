dab8108badb549f02063ffc14b619bd9724e2f59
zach <zach@joonbug.com>
2011-09-17 12:59:27 -0400

    form update

--- pages/admin/seo/duplicate-data/phrases/skybox/phrase-skybox.php
+++ pages/admin/seo/duplicate-data/phrases/skybox/phrase-skybox.php
@@ -5,32 +5,32 @@
 ?>
 		<div class="field">
 			<? $field = "phrase" ?>
-			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
 			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
 		<div class="field">
 			<? $field = "category" ?>
-			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
 			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
 		<div class="field">
 			<? $field = "sub_category" ?>
-			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
 			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
 		<div class="field">
 			<? $field = "volume" ?>
-			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
 			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
 		<div class="field">
 			<? $field = "holiday" ?>
-			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
 			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>
 		<div class="field">
 			<? $field = "keyword" ?>
-			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$o[$field]))?></label><br>
+			<label class="label" for="<?=$field?>"><?=ucwords(str_replace('_',' ',$field))?></label><br>
 			<input type="text" id="<?=$field?>" value="<?=$o[$field]?>" name="field" />
 		</div>		
 <?	
