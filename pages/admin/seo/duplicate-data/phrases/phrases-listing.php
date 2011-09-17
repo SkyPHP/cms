b953199ba1881d83d7a355d9efce2fc8608f0802
zach <zach@joonbug.com>
2011-09-17 12:06:58 -0400

    90%

--- pages/admin/seo/duplicate-data/phrases/phrases-listing.php
+++ pages/admin/seo/duplicate-data/phrases/phrases-listing.php
@@ -5,7 +5,7 @@
 		$a = array();
 		$phrases = dup_phrase_data::getList($a);
 ?>
-	<table width="1500px">
+	<table width="90%">
 		<tr class="header">
 			<th class="title">Phrase</th>
 			<th class="title">Category</th>
