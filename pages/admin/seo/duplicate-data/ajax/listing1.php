616221da5a54fe179a496ead57c8997bd792868a
zach <zach@joonbug.com>
2011-09-19 14:31:16 -0400

    filtered listing hides already checked values

--- pages/admin/seo/duplicate-data/ajax/listing1.php
+++ pages/admin/seo/duplicate-data/ajax/listing1.php
@@ -1,7 +1,6 @@
 <?
 	if (!$num) $num = 1;
 	$where=array();
-	if (is_array($_POST['ids'])) $where[] = "dup_phrase_data.id not in (".implode(',',$ids).")";	
 	if ($_POST['market']) $where[] = "market = '{$_POST['market']}'";
 	if ($_POST['market_name']) $where[] = "market_name = '{$_POST['market_name']}'";
 	if ($_POST['volume']) $where[] = "volume >= {$_POST['volume']}";
@@ -11,7 +10,7 @@
 	
 	if ($listing) foreach ($listing as $data) {
 ?>
-		<div id="listing<?=$num?>_<?=$data['phrase_id']?>">
+		<div id="listing<?=$num?>_<?=$data['phrase_id']?>" <? if (is_array($_POST['ids'])) foreach ($_POST['ids'] as $id) if ($data['phrase_id'] == $id) echo 'style="display:none;"'?>>
 			<div style="width:65px; float:left; margin-right:5px; text-align:right;">(<?=$data['volume']?$data['volume']:0?>)</div><div style="float:left; margin-right:5px;"> <input type="checkbox" id="phrase<?=$num?>_<?=$data['phrase_id']?>" phrase="<?=$data['phrase']?>" volume="<?=$data['volume']?>" phrase_id="<?=$data['phrase_id']?>" class="listing<?=$num?>-cb" id="<?=$data['lower_phrase']?>" /></div><div style="float:left; width:430px;"> <label for="phrase<?=$num?>_<?=$data['phrase_id']?>">{<?=$data['phrase_id']?>} <?=$data['lower_phrase']?></label></div>
 			<div class="clear"></div>
 		</div>
