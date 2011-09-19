2aa511172c3f83f0809c4ca22f944eee525546d8
zach <zach@joonbug.com>
2011-09-19 12:41:32 -0400

    multi fix

--- pages/admin/seo/duplicate-data/multi-listing.php
+++ pages/admin/seo/duplicate-data/multi-listing.php
@@ -60,7 +60,7 @@
 				$mods = aql::select("dup_modifier { id as mod_id, lower(phrase) as lower_phrase, phrase order by phrase asc }", array('dup_modifier'=>array('where'=>$where)));
 				if ($mods) foreach ($mods as $data) {
 ?>				
-					<input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /> <label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
+					<div><input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /> <label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
 <?	
 				}
 ?>
