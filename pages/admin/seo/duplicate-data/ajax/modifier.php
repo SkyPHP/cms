6f6e01e95f93ca22efdd5ae773ccd67bff9a983b
zach <zach@joonbug.com>
2011-09-19 12:35:08 -0400

    same

--- pages/admin/seo/duplicate-data/ajax/modifier.php
+++ pages/admin/seo/duplicate-data/ajax/modifier.php
@@ -4,7 +4,7 @@
 	$mods = aql::select("dup_modifier { id as mod_id, lower(phrase) as lower_phrase, phrase order by phrase asc }", array('dup_modifier'=>array('where'=>$where)));
 	if ($mods) foreach ($mods as $data) {
 ?>
-		<div style="width:65px; float:left; margin-right:5px; text-align:right;"></div> <div style="float:left; margin-right:5px;"><input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /></div><div style="float:left; width: 430px"><label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
+		<div><input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /> <label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
 <?	
 	}
 	else echo "No Data";
--- pages/admin/seo/duplicate-data/multi-listing.php
+++ pages/admin/seo/duplicate-data/multi-listing.php
@@ -59,8 +59,8 @@
 				if ($_POST['category']) $where[] = "( category = '{$_POST['category']}' OR category = 'general' )";
 				$mods = aql::select("dup_modifier { id as mod_id, lower(phrase) as lower_phrase, phrase order by phrase asc }", array('dup_modifier'=>array('where'=>$where)));
 				if ($mods) foreach ($mods as $data) {
-?>
-					<div style="width:65px; float:left; margin-right:5px; text-align:right;"></div> <div style="float:left; margin-right:5px;"><input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /></div><div style="float:left; width: 430px"><label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
+?>				
+					<input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /> <label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
 <?	
 				}
 ?>
