3ef9d02dc0d79ddf050c16bac3cd0b28bde1f6ef
zach <zach@joonbug.com>
2011-09-19 14:50:02 -0400

    listing update

--- pages/admin/seo/duplicate-data/phrases/phrases-listing.php
+++ pages/admin/seo/duplicate-data/phrases/phrases-listing.php
@@ -10,11 +10,14 @@
 		<tr class="header">
 			<th class="title">Phrase</th>
 			<th class="title">Category</th>
+			<th class="title">Sub Category</th>
 			<th class="title">Volume</th>
+			<th class="title">Market</th>
+			<th class="title">Market Name</th>
 			<th class="title">Keyword</th>
-			<th class="title">Holiday</th>
 			<th class="title">Base</th>
 			<th class="title">Edit</th>
+			<th class="title">Holiday</th>
 		</tr>
 <?		
 		$x = 0;
@@ -25,10 +28,13 @@
 			<tr class="<?=$x%2?'alternate':'row'?>">
 				<td class="column" valign="middle"><?=$o['phrase']?></td>
 				<td class="column" valign="middle"><?=$o['category']?></td>
+				<td class="column" valign="middle"><?=$o['sub_category']?></td>
 				<td class="column" valign="middle"><?=$o['volume']?></td>
-				<td class="column" valign="middle"><?=$o['keyword']?></td>
+				<td class="column" valign="middle"><?=$o['market']?></td>
+				<td class="column" valign="middle"><?=$o['market_name']?></td>
 				<td class="column" valign="middle"><?=$o['holiday']?></td>
 				<td class="column" valign="middle"><?=$o['base']?></td>
+				<td class="column" valign="middle"><?=$o['keyword']?></td>
 				<td class="column" valign="middle" style="text-align:center;"><input class="edit" type="button" phrase_ide="<?=$o['dup_phrase_data_ide']?>" value="edit"></td>		
 			</tr>
 <?
