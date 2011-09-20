4fb151d7272ef062359551866727e12a1b97a9af
zach <zach@joonbug.com>
2011-09-20 16:09:08 -0400

    order by

--- pages/admin/seo/duplicate-data/ajax/mods.php
+++ pages/admin/seo/duplicate-data/ajax/mods.php
@@ -2,7 +2,7 @@
 	$where = array();
 	if ($_POST['page']) $where[] = "( page = '{$_POST['page']}' OR page = 'general' )";
 	if ($_POST['modifier']) $where[] = "modifier = '{$_POST['modifier']}'";
-	$mods = aql::select("dup_modifier { id as mod_id, lower(phrase) as lower_phrase, phrase order by order by sub_category, phrase }", array('dup_modifier'=>array('where'=>$where)));
+	$mods = aql::select("dup_modifier { id as mod_id, lower(phrase) as lower_phrase, phrase order by sub_category, phrase }", array('dup_modifier'=>array('where'=>$where)));
 	if ($mods) foreach ($mods as $data) {
 ?>
 		<div><input type="checkbox" id="mod_<?=$data['mod_id']?>" phrase="<?=$data['phrase']?>" mod_id="<?=$data['mod_id']?>" class="mod-cb" id="<?=$data['lower_phrase']?>" /> <label for="mod_<?=$data['mod_id']?>">{<?=$data['mod_id']?>} <?=$data['lower_phrase']?></label></div>
