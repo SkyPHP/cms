a07d072d650c60317e33ac458e18a12c32bd9d4b
zach <zach@joonbug.com>
2011-09-20 17:32:31 -0400

    js error

--- pages/admin/seo/duplicate-data/phrase-groups/phrase-groups.js
+++ pages/admin/seo/duplicate-data/phrase-groups/phrase-groups.js
@@ -2,6 +2,6 @@
 $(function() {
 	$('.assign').live('click',function() {
 		var group_ide = $(this).attr('group_ide');
-		$.skybox('/admin/seo/duplicate-data/phrase-groups/skybox/assign/'+ide);
+		$.skybox('/admin/seo/duplicate-data/phrase-groups/skybox/assign/'+group_ide);
 	});
 })
