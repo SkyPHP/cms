eb40337e3f75c13e0cc959da252f264731a0dc19
zach <zach@joonbug.com>
2011-09-20 21:40:55 -0400

    row

--- pages/admin/seo/duplicate-data/phrases/ajax/row.php
+++ pages/admin/seo/duplicate-data/phrases/ajax/row.php
@@ -5,7 +5,7 @@
 	<td class="column" valign="middle"><?=$o['volume']?></td>
 	<td class="column" valign="middle"><?=$o['market']?></td>
 	<td class="column" valign="middle"><?=$o['market_name']?></td>
-	<td class="column" valign="middle"><?=$o['holiday']?></td>
-	<td class="column" valign="middle"><?=$o['base']?></td>
 	<td class="column" valign="middle"><?=$o['keyword']?></td>
+	<td class="column" valign="middle"><?=$o['base']?></td>
+	<td class="column" valign="middle"><?=$o['holiday']?></td>
 	<td class="column" valign="middle" style="text-align:center;"><input class="edit" type="button" href="/admin/seo/duplicate-data/phrases/skybox/phrase-skybox/" ide="<?=$o['dup_phrase_data_ide']?>" value="edit"></td>
