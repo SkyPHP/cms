93ec5b0487e55a678037bdf6768ab500a1414b60
Josh Hassara <josh.hassara@joonbug.com>
2011-11-09 17:21:23 -0500

    redid the update in set_url_specific

--- pages/admin/seo/webpage/ajax/set_url_specific.php
+++ pages/admin/seo/webpage/ajax/set_url_specific.php
@@ -23,11 +23,30 @@
 	
 
 		aql::update('website_page',array('url_specific'=>$_POST['val']),$_POST['website_page_id']);
-		sql("update website_uri_data set on_website = 1 where uri = '".$_POST['uri']."' and website_id = ".$website_id);				
+		//sql("update website_uri_data set on_website = 1 where uri = '".$_POST['uri']."' and website_id = ".$website_id);
+		$aql = "website_uri_data { where uri = '".$_POST['uri']."' and website_id = ".$website_id."}";
+		$rs = aql::select($aql);
+		foreach ($rs as $r) {
+			$website_uri_data = array(
+				'on_website' => '1'
+			);
+			$rs = aql::update( 'website_uri_data', $website_uri_data, $r['website_uri_data_id'] );
+		}
+		
 ?>
 		This page is set to URL SPECIFIC. The URL is <?=$_SERVER['HTTP_HOST'].$_POST['uri']?>
 		<input type="hidden" id="uri_enabled" value="<?=$_POST['uri']?>" />
 <?		
-	} 
-	else $update = sql("update website_uri_data set on_website = 0 where uri = '".$_POST['uri']."' and website_id = ".$website_id);
+	}
+	else {
+		//$update = sql("update website_uri_data set on_website = 0 where uri = '".$_POST['uri']."' and website_id = ".$website_id);
+		$aql = "website_uri_data { where uri = '".$_POST['uri']."' and website_id = ".$website_id."}";
+		$rs = aql::select($aql);
+		foreach ($rs as $r) {
+			$website_uri_data = array(
+				'on_website' => '0'
+			);
+			$rs = aql::update( 'website_uri_data', $website_uri_data, $r['website_uri_data_id'] );
+		}
+	}
 ?>
