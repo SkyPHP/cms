b4047319a2e374b0503c239097274318fbf8ce70
Josh Hassara <josh.hassara@joonbug.com>
2011-12-08 19:04:43 -0500

    seo year fix

--- pages/seo-vars.php
+++ pages/seo-vars.php
@@ -119,7 +119,7 @@ $website_name = $p->seo['domain'] = $website->website['domain'];
 $seo_phone =  $website->website['seo_phone'];
 
 $seo_year = date('Y');
-if ($ct_campaign_id == 1 || $ct_holiday_id == 1) $year++;
+if ($ct_campaign_id == 1 || $ct_holiday_id == 1) $seo_year++;
 
 $seo_field_array=array(
 	'html'=>array(
