84346030646499f8c6a87497304a21dee29a29e8
Stan Rozenraukh <stan@stanistan.com>
2012-02-29 12:07:44 -0500

    skybox.css works

--- templates/skybox/skybox.php
+++ templates/skybox/skybox.php
@@ -1,19 +1,27 @@
 <?
+
 if ( $template_area == 'top' ) {
+
+	if ($p->is_ajax_request) {
+		$p->css[] = '/templates/skybox/skybox.css';
+	}
+
 ?>
-<div id="skybox_template" class="has-floats" style="border: 2px #333 solid; padding: 15px;color:#000;">
-	<div id="skybox_drag_handle"></div>
-	<div id="skybox_template_title" style="float:left; font-weight:bold; margin-right: 15px;">
-		<?=$this->title?>
-	</div>
-	<div id="skybox_template_close" style="float:right;">
-		<a href="javascript:history.back()" class="noajax"><img src="/images/close-x.gif" /></a>
-	</div>
-	<div id="skybox_template_title_clear" style="clear:both; height: 15px;"></div>
+	<div id="skybox_template" class="has-floats" style="border: 2px #333 solid; padding: 15px;color:#000;">
+		<div id="skybox_drag_handle"></div>
+		<div id="skybox_template_title" style="float:left; font-weight:bold; margin-right: 15px;">
+			<?=$this->title?>
+		</div>
+		<div id="skybox_template_close" style="float:right;">
+			<a href="javascript:history.back()" class="noajax"><img src="/images/close-x.gif" /></a>
+		</div>
+		<div id="skybox_template_title_clear" style="clear:both; height: 15px;"></div>
 <?
+
 } else if ( $template_area == 'bottom' ) {
+
 ?>
-</div> 
+
+	</div> 
 <?
 }
-?>
