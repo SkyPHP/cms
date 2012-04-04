0256f5b8d278ab9ca645f0cdc3a7a1bd8f4f2bdb
Josh Hassara <josh.hassara@joonbug.com>
2012-04-04 18:27:34 -0400

    signature class and related files

---
+++ lib/signature/signature.php
@@ -0,0 +1,4 @@
+<form class="sigDisplay">
+            <canvas class="pad" width="<?=$this->width?>" height="<?=$this->height?>"></canvas>
+        <div type="text" id="sig_json" name="signature" class="output"><?=$signature_string?></div>
+</form>
