b59d74b4f0888b118e6ff49d3130b11d93b3b87f
Josh Hassara <josh.hassara@joonbug.com>
2012-04-05 13:29:41 -0400

    Remove signature class files

--- lib/signature/signature.php
+++
@@ -1,4 +0,0 @@
-<form class="sigDisplay">
-            <canvas class="pad" width="<?=$this->width?>" height="<?=$this->height?>"></canvas>
-        <div type="text" id="sig_json" name="signature" class="output"><?=$signature_string?></div>
-</form>
