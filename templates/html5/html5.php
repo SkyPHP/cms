b998b076a23d0cc4aec190266955f02f4798ae4c
Stan Rozenraukh <stan@stanistan.com>
2012-04-26 18:30:47 -0400

    cleanups and rename save_form -> aqlForm.js (consistency)

--- templates/html5/html5.php
+++ templates/html5/html5.php
@@ -1,14 +1,14 @@
 <?
 
-// hook some extra js and css into the html5 template
-if ( $template_area == 'top' ) {
-    
-    $this->template_js[] = '/templates/html5/cms-html5.js';
-    $this->template_css[] = '/templates/html5/cms-html5.css';
-
-    //saveForm
-    $this->template_js[] = '/lib/js/save_form.js';
-
+# hook some extra js and css into the html5 template
+if ($template_area == 'top') {
+	
+	$this->css[] = '/templates/html5/cms-html5.css';	
+	$this->js[] = '/templates/html5/cms-html5.js';
+	$this->js[] = '/lib/js/aqlForm.js';
+	
 }
+
 $skyphp_codebase_path = end($GLOBALS['codebase_path_arr']);
-include( $skyphp_codebase_path . 'templates/html5/html5.php' );
+
+include $skyphp_codebase_path . 'templates/html5/html5.php';
