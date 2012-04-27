25d8d134399c793392b9dd70115186526ffb62e4
Stan Rozenraukh <stan@stanistan.com>
2012-04-27 16:47:05 -0400

    adding js/css after html5 template, and we make sure that these are included before any page specific ones

--- templates/html5/html5.php
+++ templates/html5/html5.php
@@ -1,12 +1,22 @@
 <?
 
 # hook some extra js and css into the html5 template
+
 if ($template_area == 'top') {
 	
-	$this->css[] = '/templates/html5/cms-html5.css';	
-	$this->js[] = '/templates/html5/cms-html5.js';
-	$this->js[] = '/lib/js/aqlForm.js';
-	
+	$css = array(
+		'/templates/html5/cms-html5.css'
+	);
+
+	$js = array(
+		'/templates/html5/cms-html5.js',
+		'/lib/js/aqlForm.js'
+	);
+
+	# make sure that these files are the first ones loaded
+	$this->css = array_merge($css, $this->css);
+	$this->js = array_merge($js, $this->js);
+
 }
 
 $skyphp_codebase_path = end($GLOBALS['codebase_path_arr']);
