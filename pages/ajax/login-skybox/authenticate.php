382ecf955411145f5239e9715c51deeeecea928f
Will Schmid <will123195@gmail.com>
2012-06-25 09:30:51 -0400

    php tag

--- pages/ajax/login-skybox/authenticate.php
+++ pages/ajax/login-skybox/authenticate.php
@@ -1,6 +1,7 @@
-<?
+<?php
+
+// allow cross domain ajax requests
 header('Access-Control-Allow-Origin: *');
-//header("Access-Control-Allow-Headers: x-requested-with");
 
 if (is_numeric($_SESSION['login']['person_id'])) {
 	if ($_SESSION['login']['activation_required']) {
@@ -9,7 +10,7 @@ if (is_numeric($_SESSION['login']['person_id'])) {
         You must activate your account before signing in. <br />
         Click <a href="javascript:activation('<?=Login::get('person_ide')?>');">here</a> to resend activation email.
         </font>
-<?
+<?php
 		include 'pages/login/logout.php';
 	}
 	else {
