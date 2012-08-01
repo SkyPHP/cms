2386ea7191cab2aa0bddca2b90bad5d9d4983c26
Stan Rozenraukh <stan@stanistan.com>
2012-08-01 13:04:51 -0400

    person updated

--- models/person/class.person.php
+++ models/person/class.person.php
@@ -2,127 +2,280 @@
 
 class person extends Model {
 
+    /**
+     * Required Fields
+     * @var array
+     */
     public $_required_fields = array(
-        'fname' => 'First Name',
-        'lname' => 'Last Name',
+        'fname'         => 'First Name',
+        'lname'         => 'Last Name',
         'email_address' => 'Email Address'
     );
 
-    public function construct(){
-        $this->addProperty('password1', 'password2', 'current_password'); //password field 1 and 2 (validate same)
+    /**
+     * Minimum Passwor Length
+     * @var int
+     */
+    public static $min_password_length = 6;
+
+    /**
+     * Possible Errors
+     * @var array
+     */
+    public static $possible_errors = array(
+        'invalid_email_address' => array(
+            'message' => 'Invalid Email.',
+            'fields'  => array('email_address')
+        ),
+        'invalid_password1' => array(
+            'message' => 'Invalid Password.',
+            'fields'  => array('password1')
+        ),
+        'password_too_short' => array(
+            'message' => 'Choose a password of at least 6 characters.',
+            'fields'  => array('password1')
+        ),
+        'invalid_password2' => array(
+            'message' => 'You must re-enter your password',
+            'fields'  => array('password2')
+        ),
+        'password_reentered_incorrectly' => array(
+            'message' => 'Password was re-entered incorrectly. Try again.',
+            'fields'  => array('password2', 'password1')
+        ),
+        'invalid_reset_hash' => array(
+            'message' => 'Request another password reset, this token is no longer valid.',
+            'fields' => array('password_reset_hash')
+        ),
+        'invalid_current_password' => array(
+            'message' => 'The password you entered is incorrect.',
+            'fields' => array('current_password')
+        ),
+        'duplicate_account' => array(
+            'message' => 'You already have an account.',
+            'type'    => 'duplicate_account',
+            'fields' => array('email_address')
+        )
+    );
+
+    /**
+     * Adds Virtual Properties (not saved)
+     */
+    public function construct()
+    {
+        // password validation (pw1, pw2, current_password)
+        $this->addProperty(
+            'password1',
+            'password2',
+            'current_password'
+        );
     }
 
-    public function validate_email_address($val) {
-        if (!$val) return;
-        $this->_data['email_address'] = trim($val);
-    }
-
-    public function beforeCheckRequiredFields(){
-        //password validation
-        //we don't give direct access to the person's password field
-        //we take password1/password2 (and current password if it's person)
-
-        if ($this->isInsert()) {
-            if (!$this->password1 && !$this->password2) {
-                // do nothing
-            } else if ($this->password1 != $this->password2) {
-                $this->_errors[] = 'The passwords you entered do not match.';
-            } else {
-                $this->password = $this->password1;
-            }
+    /**
+     * Validates Email
+     * @param string $val
+     */
+    public function validate_email_address($val)
+    {
+        if (!$val) {
             return;
         }
 
+        if (!filter_var($val, FILTER_VALIDATE_EMAIL)) {
+            $this->addError('invalid_email_address');
+        }
 
-        if ($this->password1) { //is there a password change request?
-            if ($this->password1 != $this->password2) {
-                $this->_errors[] = "Passwords do not match";
+        if ($this->isInsert() && static::getByEmail($val)) {
+            $this->addError('duplicate_account');
+        }
+
+        $this->email_email_address = trim($val);
+    }
+
+    /**
+     * Gets a person by email address
+     * @param $email
+     */
+    public static function getByEmail($email)
+    {
+        $email = addslashes(trim($email));
+        return self::getOne(array(
+            'where' => array("email_address ILIKE '{$email}'"),
+            'order_by' => 'last_login_time DESC'
+        ));
+    }
+
+    /**
+     * validates password if the user is trying to change it using
+     * $this->password1 and $this->password2
+     * Otherwise, leaves it alone
+     * It handles inserts and updates differently
+     */
+    protected function validatePassword()
+    {
+        if ($this->isInsert()) {
+
+            if ($this->password || (!$this->password1 && !$this->password2)) {
+                // the user is NOT setting a password on insert (we allow this)
+                // for account creating for someone else (they get sent an email)
+                // that tells them to set their password
                 return;
             }
-            if (defined('PERSON_ID')) {
-                 if (PERSON_ID == $this->person_id) { //is this a user trying to change his own pw?
-                    //authenticate him (test his current_password)
-                    if ( Login::generateHash($this->current_password, $this->generateUserSalt() ) !=  aql::value( 'person.password_hash', PERSON_ID )  ) {
-                        $this->_errors[] = "Incorrect password";
-                        return;
-                    }
-                    //otherwise, self password change is valid
-                    //set it up for the set_password() method below
+
+            if (!$this->password1) {
+                $this->addError('invalid_password1');
+                return;
+            }
+
+            if (!$this->password2) {
+                $this->addError('invalid_password2');
+                return;
+            }
+
+            if (!$this->checkPasswordEntered()) {
+                return;
+            }
+
+            // set password to password1, this will automatically generate the hash etc.
+            $this->password = $this->password1;
+        }
+
+        if ($this->isUpdate()) {
+
+            // a password cannot be removed
+            $this->addRequiredFields(array(
+                'password' => 'Password'
+            ));
+
+            // the user is not attempting to change their own password via a form
+            if (!$this->password1) return;
+
+            // if passwords entered do not match
+            if (!$this->checkPasswordEntered()) return;
+
+            // need current password || password reset hash to update the password
+            // using pw1 or pw2
+            if (!$this->current_password && !$this->password_reset_hash) return;
+
+            // load person to compare original password or reset_hash
+            $o = new person($this->person_id);
+
+            if ($this->current_password) {
+
+                $hashed = Login::generateHash(
+                    $this->current_password,
+                    $this->generateUserSalt()
+                );
+
+                if ($hashed != $o->password_hash) {
+                    $this->addError('invalid_current_password');
+                } else {
                     $this->password = $this->password1;
                 }
-            } else {
-                // trying to reset your own password but not logged in
-                if ($this->password_reset_hash != aql::value( 'person.password_reset_hash', $this->person_id ) ) {
-                    $this->_errors[] = 'Request another password reset, this token is no longer valid.';
-                    $o = new person;
-                    $o->person_id = $this->person_id;
-                    $o->_token = $this->_token;
-                    $o->password_reset_hash = null;
-                    $o->save();
-                    return;
+
+            } else if ($this->password_reset_hash) {
+
+                if ($this->password_reset_hash != $o->password_reset_hash) {
+                    $this->addError('invalid_reset_hash');
+                    $this->saveProperties(array(
+                        'password_reset_hash' => null
+                    ));
+                } else {
+                    $this->password_reset_hash = null;
+                    $this->password = $this->password1;
                 }
-                $this->password = $this->password1;
-                $this->password_reset_hash = null;
+
             }
         }
-
     }
 
-    public function afterInsert() {
-        $this->_postSavePassword();
-        $this->reload();
-    }
-
-    public function generateResetHash() {
-        if (!$this->person_id) return;
-        $o = new person;
-        $o->person_id = $this->person_id;
-        $o->_token = $o->getToken();
-        $o->password_reset_hash = $this->makeResetHash();
-        $re = $o->save();
-        if ($re['status'] == 'OK') {
-            $this->password_reset_hash = $o->password_reset_hash;
+    /**
+     * Checks if pw1 == pw2 and if not, adds an error to the stack
+     * @return Bool
+     */
+    protected function checkPasswordEntered()
+    {
+        if ($this->password1 == $this->password2) {
+            return true;
         }
-        return $re;
+
+        $this->addError('password_reentered_incorrectly');
+        return false;
     }
 
-    public function makeResetHash() {
+    /**
+     * Sets Password after insert
+     */
+    public function afterInsert()
+    {
+        $this->_postSavePassword();
+    }
+
+    /**
+     * Generates a password reset hash
+     * @todo Should throw Exception if no ID
+     */
+    public function generateResetHash()
+    {
+        return ($this->getID())
+            ? $this->saveProperties(array(
+                'password_reset_hash' => $this->makeResetHash()
+            ))
+            : null;
+    }
+
+    /**
+     * Generates user salt
+     * @todo Should throw Exception if no ID
+     */
+    public function generateUserSalt()
+    {
+        return ($this->getID())
+            ? Login::generateUserSalt($this->getIDE())
+            : null;
+    }
+
+    /**
+     * Sets password
+     * @param type $val
+     */
+    public function validate_password($val)
+    {
+        if (!$val) return;
+
+        if (strlen($val) < static::$min_password_length) {
+            $this->addError('password_too_short');
+        }
+
+        // only do this during an update because we cannot generate a salt
+        // without an ID
+        if ($this->isUpdate()) {
+            $this->_data['password_hash'] = Login::generateHash(
+                $val,
+                $this->generateUserSalt()
+            );
+            // @todo: Uncomment this when we no longer have that dependency
+            // $this->_data['password'] = null;
+        }
+    }
+
+    /**
+     * Updates last login time
+     */
+    public function updateLastLoginTime()
+    {
+        $this->saveProperties(array(
+            'last_login_time' => 'now()'
+        ));
+    }
+
+    /**
+     * Creates a reset hash
+     * @return  string
+     */
+    public function makeResetHash()
+    {
         return sha1(mt_rand());
     }
-
-    private function _postSavePassword() {
-        if (!$this->password) {
-            return;
-        }
-
-        return $this->saveProperties(array(
-            'password' => $this->password
-        ));
-    }
-
-    public function generateUserSalt() {
-        if (!$this->getIDE()) return;
-        return Login::generateUserSalt($this->getIDE());
-    }
-
-    public function validate_password($val) {
-        if (!$val) return;
-        if (!$this->getIDE()) return;
-        $this->_data['password_hash'] = Login::generateHash($val, $this->generateUserSalt());
-    }
-
-    public function updateLastLoginTime() {
-        $o = new person();
-        $o->loadArray(array(
-            'person_ide' => $this->person_ide,
-            'last_login_time' => 'now()',
-            '_token' => $this->_token
-        ));
-        $re = $o->save();
-        if ($re['status'] == 'OK') $this->last_login_time = $o->last_login_time;
-    }
-
-
-
 
 }
