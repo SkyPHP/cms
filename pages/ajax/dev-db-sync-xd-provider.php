3c3d57c8f37b1da1b41efd5b055a8962adef476b
Will Schmid <will123195@gmail.com>
2012-08-12 14:56:32 -0400

    test

--- pages/ajax/dev-db-sync-xd-provider.php
+++ pages/ajax/dev-db-sync-xd-provider.php
@@ -1,54 +1,42 @@
 <?php
 
-    global $jquery_version;
+$this->template('xdm-provider', 'top');
 ?>
-<!doctype html>
-<html>
-<head>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/<?=$jquery_version?>/jquery.min.js"></script>
-    <script>!window.jQuery && document.write(unescape('%3Cscript src="/lib/js/jquery-<?=$jquery_version?>.min.js"%3E%3C/script%3E'))</script>
-</head>
-<body>
-    <script>
-        if ( typeof window.JSON === 'undefined' ) {
-            document.write('<script src="/lib/history.js-1.5/json2.min.js"><\/script>');
-        }
-    </script>
-    <script type="text/javascript" src="/lib/easyXDM/easyXDM.min.js"></script>
-    <script type="text/javascript">
-        $(function(){
 
-            var rpc = new easyXDM.Rpc({},{
-                local: {
-                    getDiffs: function(post, successFn, errorFn){
-                        // check to see if logged in
-                        $.post('/ajax/login-skybox/authenticate',function(data){
-                            if (data=='true') {
-                                // get the diffs
-                                $.post(
-                                    '/dev/db/sync/diff.json',
-                                    post,
-                                    function(data){
-                                        rpc.renderDiffs(data);
-                                        rpc.resetButton();
-                                    }
-                                );
-                            } else {
-                                rpc.resetButton();
-                                var domain = window.location.origin;
-                                alert('You need to be logged in at ' + domain);
-                            }
-                        });
-                    }
-                },
-                remote: {
-                    renderDiffs:{},
-                    resetButton:{}
+<script type="text/javascript">
+    $(function(){
+
+        var rpc = new easyXDM.Rpc({},{
+            local: {
+                getDiffs: function(post, successFn, errorFn){
+                    // check to see if logged in
+                    $.post('/ajax/login-skybox/authenticate',function(data){
+                        if (data=='true') {
+                            // get the diffs
+                            $.post(
+                                '/dev/db/sync/diff.json',
+                                post,
+                                function(data){
+                                    rpc.renderDiffs(data);
+                                    rpc.resetButton();
+                                }
+                            );
+                        } else {
+                            rpc.resetButton();
+                            var domain = window.location.origin;
+                            alert('You need to be logged in at ' + domain);
+                        }
+                    });
                 }
-            });
-
+            },
+            remote: {
+                renderDiffs:{},
+                resetButton:{}
+            }
         });
-    </script>
 
-</body>
-</html>
+    });
+</script>
+
+<?
+$this->template('xdm-provider', 'bottom');
