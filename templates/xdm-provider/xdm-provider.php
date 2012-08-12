09dfa7cce6e94cc32b739403c69397fedfd213db
Will Schmid <will123195@gmail.com>
2012-08-12 14:58:33 -0400

    xdm-provider template

--- templates/xdm-provider/xdm-provider.php
+++ templates/xdm-provider/xdm-provider.php
@@ -12,16 +12,18 @@ if ($template_area == 'top') {
     <script>!window.jQuery && document.write(unescape('%3Cscript src="/lib/js/jquery-<?=$jquery_version?>.min.js"%3E%3C/script%3E'))</script>
 </head>
 <body>
-    <script>
-        if ( typeof window.JSON === 'undefined' ) {
-            document.write('<script src="/lib/history.js-1.5/json2.min.js"><\/script>');
-        }
-    </script>
-    <script type="text/javascript" src="/lib/easyXDM/easyXDM.min.js"></script>
+<script>
+    if ( typeof window.JSON === 'undefined' ) {
+        document.write('<script src="/lib/history.js-1.5/json2.min.js"><\/script>');
+    }
+</script>
+<script type="text/javascript" src="/lib/easyXDM/easyXDM.min.js"></script>
+
 
 <?php
 } else if ($template_area == 'bottom') {
 ?>
+
 
 </body>
 </html>
