2c04864bbcd411aaf808cbd1afa1cafef5200a22
skyadmin <dev@joonbug.com>
2013-10-14 20:33:22 -0400

    Update reset-password.php

--- includes/Mailers/reset-password.php
+++ includes/Mailers/reset-password.php
@@ -8,12 +8,14 @@ $ide = $data['person']->getIDE();
 $hash = $data['person']->password_reset_hash;
 
 $this->setFrom("no-reply@$host");
+$this->setCredentials((object)['api'=>API_MANDRILL_SECRET]);
+$this->setMethod('mandrill');
 
 $uri = sprintf('http://%s/reset-password/%s/%s', $host, $ide, $hash);
 
 ?>
 
-<p>
+<p> 
 	You recently asked to reset your password. To complete your request, please follow this link:
 </p>
 
