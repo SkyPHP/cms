0153d0ca94f38ee0201c1fbd80b548a463dc179b
Andrew <aresnick62@yahoo.com>
2015-06-26 13:53:17 -0400

    New \Sky\VF\ImageManager class to grab flyers and single venue images

---
+++ lib/Sky/VF/ImageManager.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Sky\VF;
+
+/**
+ * @package SkyPHP
+ */
+class ImageManager
+{
+
+    /**
+    * Get event flyer, resized with imgix
+    */
+    public static function get_flyer($eventide, $flyer_type, $w, $h){ 
+        $imgix_base = "http://joonbug.imgix.net/";
+        $imgix_w = "";
+        $imgix_h = "";
+        if(!is_null($w)){
+            $imgix_w = "&w=".$w;
+        }
+        if(!is_null($h)){
+            $imgix_h = "&h=".$h;
+        }
+        $imgix_params = "?fit=crop".$imgix_w.$imgix_h;
+
+        $this_event = new \Crave\Model\ct_event($eventide);
+        $media_items = $this_event->media_items;
+        $media = json_decode($media_items);
+
+        $flyers = [] ;
+        if($media){
+            foreach ($media as $type => $image) {
+                $flyers[$type] = $image; 
+            } 
+        }
+        $flyer = "";
+        if(isset($flyers[$flyer_type]) && $flyers[$flyer_type] != ""){
+            $flyer = $imgix_base . "events/" . $eventide . "/". $flyer_type ."/" . $flyers[$flyer_type] . $imgix_params;
+        }
+
+        return $flyer;
+    }
+
+
+    /**
+    * Get a single venue image from the new system, with featured as priority
+    */
+    public static function get_venue_image($venueide, $w, $h){
+        $venue = new \Crave\Model\venue($venueide);
+
+        $media_items = $venue->media_items;
+        $media = json_decode($media_items);
+
+        if(!is_null($media) && !empty($media)){
+            if(stripos($media_items, 'featured":"1"')){
+                foreach($media as $image){
+                    if($image->featured == 1){
+                        $config = [
+                            'width' => $w,
+                            'height' => $h,
+                            'crop' =>true,
+                            'resize'=>true
+                        ];
+
+                        $params = [
+                            'ide'=>$venueide,
+                            'filename'=>$image->name,
+                            'config'=>$config,
+                            'type'=>'venue'
+                        ];
+
+                        $img = \vf::getItem(181847, 0, 0, 0, "v2", $params);
+                        
+                        return $img;
+                    }
+                }
+            }else{
+                $image = $media[0];
+
+                $config = [
+                    'width' => $w,
+                    'height' => $h,
+                    'crop' =>true,
+                    'resize'=>true
+                ];
+
+                $params = [
+                    'ide'=>$venueide,
+                    'filename'=>$image->name,
+                    'config'=>$config,
+                    'type'=>'venue'
+                ];
+
+                $img = \vf::getItem(181847, 0, 0, 0, "v2", $params);
+
+                return $img;
+            }
+        }
+
+        return false;
+    }
+
+}
