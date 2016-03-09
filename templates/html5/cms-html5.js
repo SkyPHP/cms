9acdadf845bb7a517d48a66494f58134585a8e0a
Brandon Wilson <brandon@emagid.com>
2016-03-09 13:27:18 -0500

    change jquery/mobile js file.

--- templates/html5/cms-html5.js
+++ templates/html5/cms-html5.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $('.pagination-limit').live('change',function() {
+    $('.pagination-limit').on('change',function() {
 
         var $this   = $(this),
             name    = $this.attr('name'),
@@ -21,7 +21,7 @@ $(function(){
 
     });
 
-    $('#skybox_error .ui-icon-circle-close').live('click', function() {
+    $('#skybox_error .ui-icon-circle-close').on('click', function() {
         $.skyboxHide();
         return false;
     });
--- templates/mobile/mobile.php
+++ templates/mobile/mobile.php
@@ -73,8 +73,8 @@ if ( $template_area == 'top' ) {
     }
 ?>
 
-    <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     
 <?php
     if (true) echo $this->javascript();
